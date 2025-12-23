117257e724b6c810ddb923236b8a7b1ac8261b51
Christo du Toit <christo@dutoit.co.uk>
2025-12-23 12:21:33 +0000

    DATA: EventV2Type

---
+++ EventHighway.Core/Models/Services/Foundations/Events/V2/EventV2Type.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace EventHighway.Core.Models.Services.Foundations.Events.V2
+{
+    public enum EventV2Type
+    {
+        Immediate,
+        Scheduled
+    }
+}
