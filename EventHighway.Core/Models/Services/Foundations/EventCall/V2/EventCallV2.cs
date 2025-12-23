ee76697dfa3bb3deb3e3a232277a01f32bcbbf37
Christo du Toit <christo@dutoit.co.uk>
2025-12-23 12:17:18 +0000

    DATA: EventCallV2

---
+++ EventHighway.Core/Models/Services/Foundations/EventCall/V2/EventCallV2.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace EventHighway.Core.Models.Services.Foundations.EventCall.V2
+{
+    internal class EventCallV2
+    {
+        public string HandlerName { get; set; }
+        public Dictionary<string, string> HandlerParams { get; set; } = new();
+        public string Content { get; set; }
+        public string Response { get; set; }
+    }
+}
