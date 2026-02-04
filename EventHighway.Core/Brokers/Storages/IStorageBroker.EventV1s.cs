18f19b9f0bd4cb5dd3015cf30bc904458ae50e56
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-02-04 07:37:07 +0100

    CODE RUB: Fix Order

--- EventHighway.Core/Brokers/Storages/IStorageBroker.EventV1s.cs
+++ EventHighway.Core/Brokers/Storages/IStorageBroker.EventV1s.cs
@@ -12,8 +12,8 @@ namespace EventHighway.Core.Brokers.Storages
     internal partial interface IStorageBroker
     {
         ValueTask<EventV1> InsertEventV1Async(EventV1 eventV1);
-        ValueTask<IQueryable<EventV1>> SelectAllEventV1sWithListenersAsync();
         ValueTask<IQueryable<EventV1>> SelectAllEventV1sAsync();
+        ValueTask<IQueryable<EventV1>> SelectAllEventV1sWithListenersAsync();
         ValueTask<EventV1> SelectEventV1ByIdAsync(Guid eventV1Id);
         ValueTask<EventV1> UpdateEventV1Async(EventV1 eventV1);
         ValueTask<EventV1> DeleteEventV1Async(EventV1 eventV1);
