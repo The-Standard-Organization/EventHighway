b4a04347c59a93bf05240f62e308094fac8ea972
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-02-04 07:45:31 +0100

    ShouldRetrieveAllEventV1sWithListenersAsync -> PASS

--- EventHighway.Core/Services/Foundations/Events/V1/EventV1Service.cs
+++ EventHighway.Core/Services/Foundations/Events/V1/EventV1Service.cs
@@ -39,9 +39,9 @@ namespace EventHighway.Core.Services.Foundations.Events.V1
         public ValueTask<IQueryable<EventV1>> RetrieveAllEventV1sAsync() =>
         TryCatch(async () => await this.storageBroker.SelectAllEventV1sAsync());
 
-        public ValueTask<IQueryable<EventV1>> RetrieveAllEventV1sWithListenersAsync()
+        public async ValueTask<IQueryable<EventV1>> RetrieveAllEventV1sWithListenersAsync()
         {
-            throw new NotImplementedException();
+            return await this.storageBroker.SelectAllEventV1sWithListenersAsync();
         }
 
         public ValueTask<EventV1> ModifyEventV1Async(EventV1 eventV1) =>
