0b9cc43c21be57dd6e8711a4860e9568ce27937b
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-02-05 15:37:03 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V1/EventV1ArchiveServiceTests.Exceptions.Add.cs
@@ -0,0 +1,69 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V1
+{
+    public partial class EventV1ArchiveServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            EventV1Archive someEventV1Archive = CreateRandomEventV1Archive();
+            SqlException sqlException = CreateSqlException();
+
+            var failedEventV1ArchiveStorageException =
+                new FailedEventV1ArchiveStorageException(
+                    message: "Failed event archive storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedEventV1ArchiveDependencyException =
+                new EventV1ArchiveDependencyException(
+                    message: "Event archive dependency error occurred, contact support.",
+                    innerException: failedEventV1ArchiveStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<EventV1Archive> addEventV1ArchiveTask =
+                this.eventV1ArchiveService.AddEventV1ArchiveAsync(someEventV1Archive);
+
+            EventV1ArchiveDependencyException actualEventV1ArchiveDependencyException =
+                await Assert.ThrowsAsync<EventV1ArchiveDependencyException>(
+                    addEventV1ArchiveTask.AsTask);
+
+            // then
+            actualEventV1ArchiveDependencyException.Should()
+                .BeEquivalentTo(expectedEventV1ArchiveDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventV1ArchiveAsync(It.IsAny<EventV1Archive>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+
+    }
+}
---
+++ EventHighway.Core/Models/Services/Foundations/EventsArchives/V1/Exceptions/FailedEventV1ArchiveStorageException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Foundations.EventsArchives.V1.Exceptions
+{
+    public class FailedEventV1ArchiveStorageException : Xeption
+    {
+        public FailedEventV1ArchiveStorageException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
