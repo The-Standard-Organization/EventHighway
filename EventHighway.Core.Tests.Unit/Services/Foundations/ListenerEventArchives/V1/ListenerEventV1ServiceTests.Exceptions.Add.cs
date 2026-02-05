7746f43292a1112b332ee90eb0f6e43ef2913557
zurakov <zafarurakov@outlook.com>
2026-02-05 21:39:54 +0800

    ShouldThrowDependencyValidationExceptionOnAddIfDuplicateKeyErrorAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEventArchives/V1/ListenerEventV1ServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEventArchives/V1/ListenerEventV1ServiceTests.Exceptions.Add.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
 using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1.Exceptions;
 using FluentAssertions;
@@ -53,6 +54,58 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEventArchive
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedListenerEventV1ArchiveDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDuplicateKeyErrorAndLogItAsync()
+        {
+            // given
+            string randomMessage = GetRandomString();
+            ListenerEventV1Archive someListenerEventV1Archive = CreateRandomListenerEventV1Archive();
+            var duplicateKeyException = new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsListenerEventV1ArchiveException =
+                new AlreadyExistsListenerEventV1ArchiveException(
+                    message: "Listener event archive with the same id already exists.",
+                    innerException: duplicateKeyException);
+
+            var expectedListenerEventV1ArchiveDependencyValidationException =
+                new ListenerEventV1ArchiveDependencyValidationException(
+                    message: "Listener event archive validation error occurred, fix the errors and try again.",
+                    innerException: alreadyExistsListenerEventV1ArchiveException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<ListenerEventV1Archive> addListenerEventV1ArchiveTask =
+                this.listenerEventV1ArchiveService.AddListenerEventV1ArchiveAsync(someListenerEventV1Archive);
+
+            ListenerEventV1ArchiveDependencyValidationException actualListenerEventV1ArchiveDependencyValidationException =
+                await Assert.ThrowsAsync<ListenerEventV1ArchiveDependencyValidationException>(
+                    addListenerEventV1ArchiveTask.AsTask);
+
+            // then
+            actualListenerEventV1ArchiveDependencyValidationException.Should()
+                .BeEquivalentTo(expectedListenerEventV1ArchiveDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventV1ArchiveDependencyValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
