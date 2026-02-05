11d84b8540fecee1d56912a96b6eb3f56421d61f
zurakov <zafarurakov@outlook.com>
2026-02-05 21:36:16 +0800

    ShouldAddListenerEventV1ArchiveAsync -> PASS

---
+++ EventHighway.Core/Services/Foundations/ListenerEventArchives/V1/ListenerEventV1ArchiveService.Exceptions.cs
@@ -0,0 +1,87 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1.Exceptions;
+using Xeptions;
+
+namespace EventHighway.Core.Services.Foundations.ListenerEventArchives.V1
+{
+    internal partial class ListenerEventV1ArchiveService
+    {
+        private delegate ValueTask<ListenerEventV1Archive> ReturningListenerEventV1ArchiveFunction();
+
+        private async ValueTask<ListenerEventV1Archive> TryCatch(
+            ReturningListenerEventV1ArchiveFunction returningListenerEventV1ArchiveFunction)
+        {
+            return await returningListenerEventV1ArchiveFunction();
+        }
+
+        private async ValueTask<ListenerEventV1ArchiveValidationException> CreateAndLogValidationExceptionAsync(
+            Xeption exception)
+        {
+            var listenerEventV1ArchiveValidationException =
+                new ListenerEventV1ArchiveValidationException(
+                    message: "Listener event archive validation error occurred, fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(listenerEventV1ArchiveValidationException);
+
+            return listenerEventV1ArchiveValidationException;
+        }
+
+        private async ValueTask<ListenerEventV1ArchiveDependencyException> CreateAndLogCriticalDependencyExceptionAsync(
+            Xeption exception)
+        {
+            var listenerEventV1ArchiveDependencyException =
+                new ListenerEventV1ArchiveDependencyException(
+                    message: "Listener event archive dependency error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(listenerEventV1ArchiveDependencyException);
+
+            return listenerEventV1ArchiveDependencyException;
+        }
+
+        private async ValueTask<ListenerEventV1ArchiveDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(
+                Xeption exception)
+        {
+            var listenerEventV1ArchiveDependencyValidationException =
+                new ListenerEventV1ArchiveDependencyValidationException(
+                    message: "Listener event archive validation error occurred, fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(listenerEventV1ArchiveDependencyValidationException);
+
+            return listenerEventV1ArchiveDependencyValidationException;
+        }
+
+        private async ValueTask<ListenerEventV1ArchiveDependencyException> CreateAndLogDependencyExceptionAsync(
+            Xeption exception)
+        {
+            var listenerEventV1ArchiveDependencyException =
+                new ListenerEventV1ArchiveDependencyException(
+                    message: "Listener event archive dependency error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(listenerEventV1ArchiveDependencyException);
+
+            return listenerEventV1ArchiveDependencyException;
+        }
+
+        private async ValueTask<ListenerEventV1ArchiveServiceException> CreateAndLogServiceExceptionAsync(Xeption exception)
+        {
+            var listenerEventV1ArchiveServiceException =
+                new ListenerEventV1ArchiveServiceException(
+                    message: "Listener event archive service error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(listenerEventV1ArchiveServiceException);
+
+            return listenerEventV1ArchiveServiceException;
+        }
+    }
+}
---
+++ EventHighway.Core/Services/Foundations/ListenerEventArchives/V1/ListenerEventV1ArchiveService.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Brokers.Storages;
+using EventHighway.Core.Brokers.Times;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
+
+namespace EventHighway.Core.Services.Foundations.ListenerEventArchives.V1
+{
+    internal partial class ListenerEventV1ArchiveService : IListenerEventV1ArchiveService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ListenerEventV1ArchiveService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<ListenerEventV1Archive> AddListenerEventV1ArchiveAsync(
+            ListenerEventV1Archive listenerEventV1Archive) => TryCatch(async () =>
+        {
+            return await storageBroker.InsertListenerEventV1ArchiveAsync(
+                listenerEventV1Archive);
+        });
+    }
+}
