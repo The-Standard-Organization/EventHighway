80b7e5ca3d0bd451d7b68d0f91266193ea438285
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-02-17 12:19:23 +0100

    ShouldAddListenerEventV1ArchiveAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEventArchives/V1/ListenerEventV1ArchiveProcessingServiceTests.Logic.Add.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEventArchives.V1
+{
+    public partial class ListenerEventV1ArchiveProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddListenerEventV1ArchiveAsync()
+        {
+            // given
+            ListenerEventV1Archive randomListenerEventV1Archive =
+                CreateRandomListenerEventV1Archive();
+
+            ListenerEventV1Archive inputListenerEventV1Archive =
+                randomListenerEventV1Archive;
+
+            ListenerEventV1Archive storageListenerEventV1Archive =
+                inputListenerEventV1Archive;
+
+            ListenerEventV1Archive expectedListenerEventV1Archive =
+                storageListenerEventV1Archive.DeepClone();
+
+            this.listenerEventV1ArchiveServiceMock.Setup(broker =>
+                broker.AddListenerEventV1ArchiveAsync(inputListenerEventV1Archive))
+                    .ReturnsAsync(storageListenerEventV1Archive);
+
+            // when
+            ListenerEventV1Archive actualListenerEventV1Archive =
+                await this.listenerEventV1ArchiveProcessingService
+                    .AddListenerEventV1ArchiveAsync(
+                        inputListenerEventV1Archive);
+
+            // then
+            actualListenerEventV1Archive.Should().BeEquivalentTo(
+                expectedListenerEventV1Archive);
+
+            this.listenerEventV1ArchiveServiceMock.Verify(broker =>
+                broker.AddListenerEventV1ArchiveAsync(
+                    inputListenerEventV1Archive),
+                        Times.Once);
+
+            this.listenerEventV1ArchiveServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEventArchives/V1/ListenerEventV1ArchiveProcessingServiceTests.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using EventHighway.Core.Brokers.Loggings;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
+using EventHighway.Core.Services.Foundations.ListenerEventArchives.V1;
+using EventHighway.Core.Services.Processings.ListenerEventArchives.V1;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEventArchives.V1
+{
+    public partial class ListenerEventV1ArchiveProcessingServiceTests
+    {
+        private readonly Mock<IListenerEventV1ArchiveService> listenerEventV1ArchiveServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IListenerEventV1ArchiveProcessingService listenerEventV1ArchiveProcessingService;
+
+        public ListenerEventV1ArchiveProcessingServiceTests()
+        {
+            this.listenerEventV1ArchiveServiceMock =
+                new Mock<IListenerEventV1ArchiveService>();
+
+            this.loggingBrokerMock =
+                new Mock<ILoggingBroker>();
+
+            this.listenerEventV1ArchiveProcessingService = new ListenerEventV1ArchiveProcessingService(
+                listenerEventV1ArchiveService: this.listenerEventV1ArchiveServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static Guid GetRandomId() =>
+            Guid.NewGuid();
+
+        private static int GetRandomNegativeNumber() =>
+            -1 * GetRandomNumber();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 9).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static ListenerEventV1Archive CreateRandomListenerEventV1Archive() =>
+            CreateListenerEventV1ArchiveFiller().Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<ListenerEventV1Archive> CreateListenerEventV1ArchiveFiller()
+        {
+            var filler = new Filler<ListenerEventV1Archive>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset);
+
+            return filler;
+        }
+    }
+}
