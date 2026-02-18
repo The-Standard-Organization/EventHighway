5d83b4bf23d8eef0adde44d394f2eeff0ee640cd
ZURAKOV <ZafarUrakov@outlook.com>
2026-02-18 16:00:42 +0300

    ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V1.Exceptions;
 using EventHighway.Core.Models.Services.Foundations.Events.V1;
@@ -107,6 +108,59 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedEventV1ArchiveOrchestrationDependencyException))),
+                        Times.Once);
+
+            this.eventV1ArchiveServiceMock.Verify(broker =>
+                broker.AddEventV1ArchiveAsync(It.IsAny<EventV1Archive>()),
+                    Times.Never);
+
+            this.listenerEventV1ArchiveServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventV1ArchiveServiceMock.VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            EventV1Archive someEventV1Archive = CreateRandomEventV1Archive();
+            var exception = new Exception();
+
+            var failedEventV1ArchiveOrchestrationServiceException =
+                new FailedEventV1ArchiveOrchestrationServiceException(
+                    message: "Failed event archive service error occurred, contact support.",
+                    innerException: exception);
+
+            var expectedEventV1ArchiveOrchestrationServiceException =
+                new EventV1ArchiveOrchestrationServiceException(
+                    message: "Event archive service error occurred, contact support.",
+                    innerException: failedEventV1ArchiveOrchestrationServiceException);
+
+            this.listenerEventV1ArchiveServiceMock.Setup(service =>
+                service.AddListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()))
+                    .ThrowsAsync(exception);
+
+            // when
+            ValueTask addEventV1ArchiveTask =
+                this.eventV1ArchiveOrchestrationService.AddEventV1ArchiveWithListenerEventV1ArchivesAsync(
+                    someEventV1Archive);
+
+            EventV1ArchiveOrchestrationServiceException
+                actualEventV1ArchiveOrchestrationServiceException =
+                    await Assert.ThrowsAsync<EventV1ArchiveOrchestrationServiceException>(
+                        addEventV1ArchiveTask.AsTask);
+
+            // then
+            actualEventV1ArchiveOrchestrationServiceException.Should()
+                .BeEquivalentTo(expectedEventV1ArchiveOrchestrationServiceException);
+
+            this.listenerEventV1ArchiveServiceMock.Verify(service =>
+                service.AddListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveOrchestrationServiceException))),
                         Times.Once);
 
             this.eventV1ArchiveServiceMock.Verify(broker =>
