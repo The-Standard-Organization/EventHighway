2f79881bbaed8dba44bb35aad6abdeedb11f9e38
ZURAKOV <ZafarUrakov@outlook.com>
2026-02-18 15:49:32 +0300

    ShouldThrowValidationExceptionOnAddIfListenerEventV1ArchivesAreNullAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Validations.Add.cs
@@ -16,7 +16,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
     public partial class EventV1ArchiveOrchestrationServiceTests
     {
         [Fact]
-        public async Task ShouldThrowValidationExceptionOnSubmitIfEventV1ArchiveIsNullAndLogItAsync()
+        public async Task ShouldThrowValidationExceptionOnAddIfEventV1ArchiveIsNullAndLogItAsync()
         {
             // given
             EventV1Archive nullEventV1Archive = null;
@@ -31,14 +31,65 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
                     innerException: nullEventV1ArchiveOrchestrationException);
 
             // when
-            ValueTask submitEventV1ArchiveTask =
+            ValueTask addEventV1ArchiveTask =
                 this.eventV1ArchiveOrchestrationService.AddEventV1ArchiveWithListenerEventV1ArchivesAsync(
                     nullEventV1Archive);
 
             EventV1ArchiveOrchestrationValidationException
                 actualEventV1ArchiveOrchestrationValidationException =
                     await Assert.ThrowsAsync<EventV1ArchiveOrchestrationValidationException>(
-                        submitEventV1ArchiveTask.AsTask);
+                        addEventV1ArchiveTask.AsTask);
+
+            // then
+            actualEventV1ArchiveOrchestrationValidationException.Should().BeEquivalentTo(
+                expectedEventV1ArchiveOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveOrchestrationValidationException))),
+                        Times.Once);
+
+            this.listenerEventV1ArchiveServiceMock.Verify(service =>
+                service.AddListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()),
+                    Times.Never);
+
+            this.eventV1ArchiveServiceMock.Verify(broker =>
+                broker.AddEventV1ArchiveAsync(
+                    It.IsAny<EventV1Archive>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.listenerEventV1ArchiveServiceMock.VerifyNoOtherCalls();
+            this.eventV1ArchiveServiceMock.VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfListenerEventV1ArchivesAreNullAndLogItAsync()
+        {
+            // given
+            var invalidEventV1Archive = new EventV1Archive
+            {
+                ListenerEventV1Archives = null
+            };
+
+            var nullEventV1ArchiveOrchestrationException =
+                new NullListenerEventV1ArchivesOrchestrationException(
+                    message: "Listener event archives are null.");
+
+            var expectedEventV1ArchiveOrchestrationValidationException =
+                new EventV1ArchiveOrchestrationValidationException(
+                    message: "Event archive validation error occurred, fix the errors and try again.",
+                    innerException: nullEventV1ArchiveOrchestrationException);
+
+            // when
+            ValueTask addEventV1ArchiveTask =
+                this.eventV1ArchiveOrchestrationService.AddEventV1ArchiveWithListenerEventV1ArchivesAsync(
+                    invalidEventV1Archive);
+
+            EventV1ArchiveOrchestrationValidationException
+                actualEventV1ArchiveOrchestrationValidationException =
+                    await Assert.ThrowsAsync<EventV1ArchiveOrchestrationValidationException>(
+                        addEventV1ArchiveTask.AsTask);
 
             // then
             actualEventV1ArchiveOrchestrationValidationException.Should().BeEquivalentTo(
