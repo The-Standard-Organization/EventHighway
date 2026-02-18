98de98781b06c826f09be14cab7acf4da89bffa1
ZURAKOV <ZafarUrakov@outlook.com>
2026-02-18 15:55:30 +0300

    ShouldThrowDependencyValidationExceptionOnAddIfValidationExceptionOccursAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Exceptions.Add.cs
@@ -0,0 +1,71 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V1.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.Events.V1;
+using EventHighway.Core.Models.Services.Foundations.Events.V1.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1;
+using EventHighway.Core.Models.Services.Orchestrations.EventArchives.V1;
+using EventHighway.Core.Models.Services.Orchestrations.Events.V1.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
+{
+    public partial class EventV1ArchiveOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(EventV1ArchiveValidationExceptions))]
+        [MemberData(nameof(ListenerEventV1ArchiveDependencyExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfValidationExceptionOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            EventV1Archive someEventV1Archive = CreateRandomEventV1Archive();
+
+            var expectedEventV1ArchiveOrchestrationDependencyValidationException =
+                new EventV1ArchiveOrchestrationDependencyValidationException(
+                    message: "Event archive validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.listenerEventV1ArchiveServiceMock.Setup(service =>
+                service.AddListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask addEventV1ArchiveTask =
+                this.eventV1ArchiveOrchestrationService.AddEventV1ArchiveWithListenerEventV1ArchivesAsync(
+                    someEventV1Archive);
+
+            EventV1ArchiveOrchestrationDependencyValidationException
+                actualEventV1ArchiveOrchestrationDependencyValidationException =
+                    await Assert.ThrowsAsync<EventV1ArchiveOrchestrationDependencyValidationException>(
+                        addEventV1ArchiveTask.AsTask);
+
+            // then
+            actualEventV1ArchiveOrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV1ArchiveOrchestrationDependencyValidationException);
+
+            this.listenerEventV1ArchiveServiceMock.Verify(service =>
+                service.AddListenerEventV1ArchiveAsync(It.IsAny<ListenerEventV1Archive>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveOrchestrationDependencyValidationException))),
+                        Times.Once);
+
+            this.eventV1ArchiveServiceMock.Verify(broker =>
+                broker.AddEventV1ArchiveAsync(It.IsAny<EventV1Archive>()),
+                    Times.Never);
+
+            this.listenerEventV1ArchiveServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventV1ArchiveServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.cs
@@ -7,6 +7,9 @@ using System.Linq.Expressions;
 using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Models.Services.Foundations.Events.V1;
 using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V1.Exceptions;
+using EventHighway.Core.Models.Services.s.Events.V1.Exceptions;
 using EventHighway.Core.Services.Foundations.EventArchives.V1;
 using EventHighway.Core.Services.Foundations.ListenerEventArchives.V1;
 using EventHighway.Core.Services.Orchestrations.EventArchives.V1;
@@ -34,6 +37,77 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
                 eventV1ArchiveService: this.eventV1ArchiveServiceMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
+
+        public static TheoryData<Xeption> EventV1ArchiveValidationExceptions()
+        {
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            return new TheoryData<Xeption>
+            {
+                new EventV1ArchiveValidationException(
+                    someMessage,
+                    someInnerException),
+
+                new EventV1ArchiveDependencyValidationException(
+                    someMessage,
+                    someInnerException),
+            };
+        }
+
+        public static TheoryData<Xeption> EventV1ArchiveDependencyExceptions()
+        {
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            return new TheoryData<Xeption>
+            {
+                new EventV1ArchiveDependencyException(
+                    someMessage,
+                    someInnerException),
+
+                new EventV1ArchiveServiceException(
+                    someMessage,
+                    someInnerException),
+            };
+        }
+
+        public static TheoryData<Xeption> ListenerEventV1ArchiveValidationExceptions()
+        {
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            return new TheoryData<Xeption>
+            {
+                new ListenerEventV1ArchiveValidationException(
+                    someMessage,
+                    someInnerException),
+
+                new ListenerEventV1ArchiveDependencyValidationException(
+                    someMessage,
+                    someInnerException),
+            };
+        }
+
+        public static TheoryData<Xeption> ListenerEventV1ArchiveDependencyExceptions()
+        {
+            string someMessage = GetRandomString();
+            var someInnerException = new Xeption();
+
+            return new TheoryData<Xeption>
+            {
+                new ListenerEventV1ArchiveDependencyException(
+                    someMessage,
+                    someInnerException),
+
+                new ListenerEventV1ArchiveServiceException(
+                    someMessage,
+                    someInnerException),
+            };
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
 
         private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
             actualException => actualException.SameExceptionAs(expectedException);
