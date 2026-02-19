2c7b8b3d42eae207a7728b78f15270d528bac7fa
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-02-19 15:06:21 +0100

    ShouldThrowDependencyValidationExceptionOnArchiveIfDependencyValidationErrorOccursAsync -> PASS

--- EventHighway.Core/Clients/Events/V1/EventV1sClientV1.cs
+++ EventHighway.Core/Clients/Events/V1/EventV1sClientV1.cs
@@ -3,7 +3,10 @@
 // ----------------------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Clients.Events.V1.Exceptions;
+using EventHighway.Core.Models.Services.Coordinations.Events.V1.Exceptions;
 using EventHighway.Core.Services.Coordinations.Events.V1;
+using Xeptions;
 
 namespace EventHighway.Core.Clients.Events.V1
 {
@@ -16,7 +19,41 @@ namespace EventHighway.Core.Clients.Events.V1
 
         public async ValueTask ArchiveDeadEventV1sAsync()
         {
-            await this.eventV1CoordinationServiceV1.ArchiveDeadEventV1sAsync();
+            try
+            {
+                await this.eventV1CoordinationServiceV1.ArchiveDeadEventV1sAsync();
+            }
+            catch (EventV1CoordinationDependencyValidationException
+                eventV1CoordinationDependencyValidationException)
+            {
+                throw CreateEventV1ClientDependencyValidationException(
+                    eventV1CoordinationDependencyValidationException.InnerException
+                        as Xeption);
+            }
+        }
+
+        private static EventV1ClientDependencyValidationException CreateEventV1ClientDependencyValidationException(
+            Xeption innerException)
+        {
+            return new EventV1ClientDependencyValidationException(
+                message: "Event client validation error occurred, fix the errors and try again.",
+                innerException: innerException);
+        }
+
+        private static EventV1ClientDependencyException CreateEventV1ClientDependencyException(
+            Xeption innerException)
+        {
+            return new EventV1ClientDependencyException(
+                message: "Event client dependency error occurred, contact support.",
+                innerException: innerException);
+        }
+
+        private static EventV1ClientServiceException CreateEventV1ClientServiceException(
+            Xeption innerException)
+        {
+            return new EventV1ClientServiceException(
+                message: "Event client service error occurred, contact support.",
+                innerException: innerException);
         }
     }
 }
