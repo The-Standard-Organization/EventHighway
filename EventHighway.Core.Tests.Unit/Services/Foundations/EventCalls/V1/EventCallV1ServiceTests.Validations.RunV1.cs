8743473953bcb392411e66590fadb7c1799637db
zurakov <zafarurakov@outlook.com>
2026-03-01 15:03:16 +0300

    ShouldThrowValidationExceptionOnRunV1IfHttpResponseMessageIsNullAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V1/EventCallV1ServiceTests.Validations.RunV1.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V1/EventCallV1ServiceTests.Validations.RunV1.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.Net.Http;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventCall.V1;
 using EventHighway.Core.Models.Services.Foundations.EventCall.V1.Exceptions;
@@ -49,6 +50,61 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V1
                     It.IsAny<string>(),
                     It.IsAny<string>()),
                         Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRunV1IfHttpResponseMessageIsNullAndLogItAsync()
+        {
+            // given
+            EventCallV1 randomEventCallV1 =
+                CreateRandomEventCallV1();
+
+            EventCallV1 inputEventCallV1 =
+                randomEventCallV1;
+
+            HttpResponseMessage nullHttpResponseMessage = null;
+
+            var nullHttpResponseMessageException =
+                new NullHttpResponseMessageException(message: "Http response message is null.");
+
+            var expectedEventCallV1ValidationException =
+                new EventCallV1ValidationException(
+                    message: "Event call validation error occurred, fix the errors and try again.",
+                    innerException: nullHttpResponseMessageException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostAsyncV1(
+                    inputEventCallV1.Content,
+                    inputEventCallV1.Endpoint,
+                    inputEventCallV1.Secret))
+                        .ReturnsAsync(nullHttpResponseMessage);
+
+            // when
+            ValueTask<EventCallV1> runEventCallV1Task =
+                this.eventCallV1Service.RunEventCallV1AsyncV1(inputEventCallV1);
+
+            EventCallV1ValidationException actualEventCallV1ValidationException =
+                await Assert.ThrowsAsync<EventCallV1ValidationException>(
+                    runEventCallV1Task.AsTask);
+
+            // then
+            actualEventCallV1ValidationException.Should().BeEquivalentTo(
+                expectedEventCallV1ValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventCallV1ValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostAsyncV1(
+                    inputEventCallV1.Content,
+                    inputEventCallV1.Endpoint,
+                    inputEventCallV1.Secret),
+                        Times.Once);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.apiBrokerMock.VerifyNoOtherCalls();
