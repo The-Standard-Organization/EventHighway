0d7265d2732e1afb7ce9bd94f8e6f745f8f8e2f6
zurakov <zafarurakov@outlook.com>
2026-03-01 15:04:05 +0300

    ShouldThrowCriticalDependencyExceptionOnRunV1IfCriticalDependencyErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V1/EventCallV1ServiceTests.Exceptions.RunV1.cs
@@ -0,0 +1,69 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V1;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V1.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V1
+{
+    public partial class EventCallV1ServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRunV1IfCriticalDependencyErrorOccursAndLogItAsync(
+            Xeption criticalDependencyException)
+        {
+            // given
+            EventCallV1 someEventCallV1 = CreateRandomEventCallV1();
+
+            var failedEventCallV1ConfigurationException =
+                new FailedEventCallV1ConfigurationException(
+                    message: "Failed event call configuration error occurred, contact support.",
+                    innerException: criticalDependencyException);
+
+            var expectedEventCallV1DependencyException =
+                new EventCallV1DependencyException(
+                    message: "Event call dependency error occurred, contact support.",
+                    innerException: failedEventCallV1ConfigurationException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostAsyncV1(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                        .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<EventCallV1> runEventCallV1Task =
+                this.eventCallV1Service.RunEventCallV1AsyncV1(someEventCallV1);
+
+            EventCallV1DependencyException actualEventCallV1DependencyException =
+                await Assert.ThrowsAsync<EventCallV1DependencyException>(
+                    runEventCallV1Task.AsTask);
+
+            // then
+            actualEventCallV1DependencyException.Should()
+                .BeEquivalentTo(expectedEventCallV1DependencyException);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostAsyncV1(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedEventCallV1DependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
