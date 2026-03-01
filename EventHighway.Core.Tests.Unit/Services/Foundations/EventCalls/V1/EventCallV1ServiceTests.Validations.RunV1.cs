0f174ab138aeb2de9beca609dfea9a86383aa4c6
zurakov <zafarurakov@outlook.com>
2026-03-01 15:00:35 +0300

    ShouldThrowValidationExceptionOnAddV1IfEventCallV1IsNullAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V1/EventCallV1ServiceTests.Validations.RunV1.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V1;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V1.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V1
+{
+    public partial class EventCallV1ServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddV1IfEventCallV1IsNullAndLogItAsync()
+        {
+            // given
+            EventCallV1 nullEventCallV1 = null;
+
+            var nullEventCallV1Exception =
+                new NullEventCallV1Exception(message: "Event call is null.");
+
+            var expectedEventCallV1ValidationException =
+                new EventCallV1ValidationException(
+                    message: "Event call validation error occurred, fix the errors and try again.",
+                    innerException: nullEventCallV1Exception);
+
+            // when
+            ValueTask<EventCallV1> runEventCallV1Task =
+                this.eventCallV1Service.RunEventCallV1AsyncV1(nullEventCallV1);
+
+            EventCallV1ValidationException actualEventCallV1ValidationException =
+                await Assert.ThrowsAsync<EventCallV1ValidationException>(
+                    runEventCallV1Task.AsTask);
+
+            // then
+            actualEventCallV1ValidationException.Should().BeEquivalentTo(
+                expectedEventCallV1ValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventCallV1ValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostAsyncV1(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
