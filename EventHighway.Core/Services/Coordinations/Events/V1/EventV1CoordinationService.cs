852403d8bc2d1c469ce404872df59fd68b3cfc06
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2026-03-02 11:45:29 +0100

    MAJOR CODE RUB: Revert Retry Logic on Submit EventV1 Implementation

--- EventHighway.Core/Services/Coordinations/Events/V1/EventV1CoordinationService.cs
+++ EventHighway.Core/Services/Coordinations/Events/V1/EventV1CoordinationService.cs
@@ -37,7 +37,7 @@ namespace EventHighway.Core.Services.Coordinations.Events.V1
         }
 
         public ValueTask<EventV1> SubmitEventV1Async(EventV1 eventV1) =>
-        TryCatchWithRetryAsync(returningEventV1Function: async () =>
+        TryCatch(async () =>
         {
             ValidateEventV1IsNotNull(eventV1);
 
@@ -62,20 +62,7 @@ namespace EventHighway.Core.Services.Coordinations.Events.V1
                 await ProcessEventListenerV1sAsync(submittedEventV1);
 
             return submittedEventV1;
-        },
-
-        retryEventV1Function: async () =>
-        {
-            if (eventV1.RetryAttempts > 0)
-            {
-                eventV1.RetryAttempts--;
-
-                return await SubmitEventV1Async(eventV1);
-            }
-
-            return null;
-        }
-    );
+        });
 
         public ValueTask<EventV1> SubmitEventV1AsyncV1(EventV1 eventV1) =>
         TryCatchWithRetryAsync(returningEventV1Function: async () =>
