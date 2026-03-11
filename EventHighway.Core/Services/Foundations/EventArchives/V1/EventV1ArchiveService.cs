1c962ebe5e9ed3444b266b583f4a1c2cd05ac327
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-11 16:52:06 +0100

    ShouldRetrieveAllEventV1ArchivesAsync -> PASS

--- EventHighway.Core/Services/Foundations/EventArchives/V1/EventV1ArchiveService.cs
+++ EventHighway.Core/Services/Foundations/EventArchives/V1/EventV1ArchiveService.cs
@@ -35,10 +35,10 @@ namespace EventHighway.Core.Services.Foundations.EventArchives.V1
 
             return await this.storageBroker.InsertEventV1ArchiveAsync(eventV1Archive);
         });
-
-        public ValueTask<IQueryable<EventV1Archive>> RetrieveAllEventV1ArchivesAsync()
+        
+        public async ValueTask<IQueryable<EventV1Archive>> RetrieveAllEventV1ArchivesAsync()
         {
-            throw new NotImplementedException();
+            return await this.storageBroker.SelectAllEventV1ArchivesAsync();
         }
 
     }
