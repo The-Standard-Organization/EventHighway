7c90abc1cb9395e79fe2576427f7be04f740e59e
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-16 19:50:59 +0100

    ShouldThrowValidationExceptionOnRemoveByIdIfEventV1ArchiveIsNotFoundAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V1/EventV1ArchiveServiceTests.Validations.RemoveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V1/EventV1ArchiveServiceTests.Validations.RemoveById.cs
@@ -59,5 +59,57 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V1
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdIfEventV1ArchiveIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid nonExistingEventV1ArchiveId = GetRandomId();
+            EventV1Archive nullEventV1Archive = null;
+
+            var notFoundEventV1ArchiveException =
+                new NotFoundEventV1ArchiveException(
+                    message: $"Could not find event archive with id: {nonExistingEventV1ArchiveId}.");
+
+            var expectedEventV1ArchiveValidationException =
+                new EventV1ArchiveValidationException(
+                    message: "Event archive validation error occurred, fix the errors and try again.",
+                    innerException: notFoundEventV1ArchiveException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectEventV1ArchiveByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(nullEventV1Archive);
+
+            // when
+            ValueTask<EventV1Archive> removeEventV1ArchiveByIdTask =
+                this.eventV1ArchiveService.RemoveEventV1ArchiveByIdAsync(nonExistingEventV1ArchiveId);
+
+            EventV1ArchiveValidationException actualEventV1ArchiveValidationException =
+                await Assert.ThrowsAsync<EventV1ArchiveValidationException>(
+                    removeEventV1ArchiveByIdTask.AsTask);
+
+            // then
+            actualEventV1ArchiveValidationException.Should()
+                .BeEquivalentTo(expectedEventV1ArchiveValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectEventV1ArchiveByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteEventV1ArchiveAsync(
+                    It.IsAny<EventV1Archive>()),
+                        Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
