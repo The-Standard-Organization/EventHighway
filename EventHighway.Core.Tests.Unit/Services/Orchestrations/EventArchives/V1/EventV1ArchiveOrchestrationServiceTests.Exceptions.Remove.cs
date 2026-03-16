3ca6ce24e4cba7129cbaf6158c4e327a625d17df
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-16 20:24:17 +0100

    ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Exceptions.Remove.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V1/EventV1ArchiveOrchestrationServiceTests.Exceptions.Remove.cs
@@ -123,5 +123,66 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V1
             this.eventV1ArchiveServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            DateTimeOffset inputDateTimeOffset = randomDateTimeOffset;
+
+            ArchiveDeletionPolicy someArchiveDeletionType =
+                GetValidEnum<ArchiveDeletionPolicy>();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(inputDateTimeOffset);
+
+            var serviceException = new Exception();
+
+            var failedEventV1ArchiveOrchestrationServiceException =
+                new FailedEventV1ArchiveOrchestrationServiceException(
+                    message: "Failed event archive service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedEventV1ArchiveOrchestrationExceptionException =
+                new EventV1ArchiveOrchestrationServiceException(
+                    message: "Event archive service error occurred, contact support.",
+                    innerException: failedEventV1ArchiveOrchestrationServiceException);
+
+            this.eventV1ArchiveServiceMock.Setup(service =>
+                service.RemoveEventV1ArchivesAsync(It.IsAny<DateTimeOffset>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask addEventV1ArchiveTask =
+                this.eventV1ArchiveOrchestrationService.RemoveEventV1ArchivesAsync(
+                    someArchiveDeletionType);
+
+            EventV1ArchiveOrchestrationServiceException
+                actualEventV1ArchiveOrchestrationServiceException =
+                    await Assert.ThrowsAsync<EventV1ArchiveOrchestrationServiceException>(
+                        addEventV1ArchiveTask.AsTask);
+
+            // then
+            actualEventV1ArchiveOrchestrationServiceException.Should().BeEquivalentTo(
+                expectedEventV1ArchiveOrchestrationExceptionException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.eventV1ArchiveServiceMock.Verify(service =>
+                service.RemoveEventV1ArchivesAsync(It.IsAny<DateTimeOffset>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveOrchestrationExceptionException))),
+                        Times.Once);
+
+            this.eventV1ArchiveServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
