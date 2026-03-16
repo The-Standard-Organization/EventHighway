fe99303825f79e15eb0154d73c02063ad54ac4f9
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-16 21:04:52 +0100

    ShouldThrowDependencyValidationOnCleanUpArchiveDeadEventV1sIfDependencyValidationErrorAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/V1/EventV1CoordinationServiceV1Tests.Exceptions.CleanupArchive.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Coordinations.Events.V1.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Coordinations.V1
+{
+    public partial class EventV1CoordinationServiceV1Tests
+    {
+        [Theory]
+        [MemberData(nameof(EventV1ValidationExceptions))]
+        [MemberData(nameof(EventV1ArchiveValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnCleanUpArchiveDeadEventV1sIfDependencyValidationErrorAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            var expectedEventV1CoordinationDependencyValidationException =
+                new EventV1CoordinationDependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventV1ArchiveOrchestrationServiceMock.Setup(service =>
+                service.RemoveEventV1ArchivesByDeletionPolicyAsync(ArchiveDeletionPolicy.Monthly))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask archiveDeadEventV1sTask =
+                this.eventV1CoordinationServiceV1.CleanUpArchiveDeadEventV1sAsync(ArchiveDeletionPolicy.Monthly);
+
+            EventV1CoordinationDependencyValidationException
+                actualEventV1CoordinationDependencyValidationException =
+                    await Assert.ThrowsAsync<EventV1CoordinationDependencyValidationException>(
+                        archiveDeadEventV1sTask.AsTask);
+
+            // then
+            actualEventV1CoordinationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV1CoordinationDependencyValidationException);
+
+            this.eventV1ArchiveOrchestrationServiceMock.Verify(service =>
+                service.RemoveEventV1ArchivesByDeletionPolicyAsync(ArchiveDeletionPolicy.Monthly),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV1CoordinationDependencyValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.eventV1ArchiveOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
