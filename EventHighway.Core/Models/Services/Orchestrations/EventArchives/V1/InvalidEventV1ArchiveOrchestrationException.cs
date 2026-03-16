bba30aab9a005cfdb1d14a52d96d6c25f9527565
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-16 20:15:37 +0100

    CODERUB: Added InvalidEventV1ArchiveOrchestrationException

---
+++ EventHighway.Core/Models/Services/Orchestrations/EventArchives/V1/InvalidEventV1ArchiveOrchestrationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace EventHighway.Core.Models.Services.Orchestrations.EventArchives.V1
+{
+    public partial class InvalidEventV1ArchiveOrchestrationException : Xeption
+    {
+        public InvalidEventV1ArchiveOrchestrationException(string message)
+            : base(message)
+        { }
+    }
+}
