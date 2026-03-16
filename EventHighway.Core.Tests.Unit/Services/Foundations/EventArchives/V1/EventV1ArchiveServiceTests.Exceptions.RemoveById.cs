e33f28bed1b776be5c1cf2954760b846efdb5bb6
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-16 19:52:36 +0100

    ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V1/EventV1ArchiveServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,66 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V1.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V1
+{
+    public partial class EventV1ArchiveServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someEventV1ArchiveId = GetRandomId();
+            SqlException sqlException = CreateSqlException();
+
+            var failedEventV1ArchiveStorageException =
+                new FailedEventV1ArchiveStorageException(
+                    message: "Failed event archive storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedEventV1ArchiveDependencyException =
+                new EventV1ArchiveDependencyException(
+                    message: "Event archive dependency error occurred, contact support.",
+                    innerException: failedEventV1ArchiveStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectEventV1ArchiveByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<EventV1Archive> removeEventV1ArchiveByIdTask =
+                this.eventV1ArchiveService.RemoveEventV1ArchiveByIdAsync(
+                    someEventV1ArchiveId);
+
+            EventV1ArchiveDependencyException actualEventV1ArchiveDependencyException =
+                await Assert.ThrowsAsync<EventV1ArchiveDependencyException>(
+                    removeEventV1ArchiveByIdTask.AsTask);
+
+            // then
+            actualEventV1ArchiveDependencyException.Should()
+                .BeEquivalentTo(expectedEventV1ArchiveDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectEventV1ArchiveByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedEventV1ArchiveDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
