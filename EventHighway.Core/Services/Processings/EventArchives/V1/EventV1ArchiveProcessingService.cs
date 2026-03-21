3f022f643e03dca9768c5638a7c206527d9d8986
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-03-21 20:32:07 +0100

    ShouldThrowValidationExceptionOnRemoveByIdIfEventV1ArchiveIdIsInvalidAndLogItAsync -> PASS

--- EventHighway.Core/Services/Processings/EventArchives/V1/EventV1ArchiveProcessingService.cs
+++ EventHighway.Core/Services/Processings/EventArchives/V1/EventV1ArchiveProcessingService.cs
@@ -44,10 +44,13 @@ namespace EventHighway.Core.Services.Processings.EventArchives.V1
                 eventV1ArchiveId);
         });
 
-        public async ValueTask<EventV1Archive> RemoveEventV1ArchiveByIdAsync(Guid eventV1ArchiveId)
+        public ValueTask<EventV1Archive> RemoveEventV1ArchiveByIdAsync(Guid eventV1ArchiveId) =>
+        TryCatch(async () =>
         {
+            ValidateEventV1ArchiveId(eventV1ArchiveId);
+
             return await this.eventV1ArchiveService.RemoveEventV1ArchiveByIdAsync(
                 eventV1ArchiveId);
-        }
+        });
     }
 }
