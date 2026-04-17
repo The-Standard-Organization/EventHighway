f4ca9767c130e0debd7c5d1a5e32988c16d93bec
Christo du Toit <christo@dutoit.co.uk>
2026-04-17 19:44:41 +0100

    ShouldThrowValidationExceptionOnAddIfHandlerConfigurationIsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/HandlerConfigurations/HandlerConfigurationServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/HandlerConfigurations/HandlerConfigurationServiceTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations;
 using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations.Exceptions;
@@ -30,6 +31,67 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.HandlerConfiguration
             // when
             ValueTask<HandlerConfiguration> addHandlerConfigurationTask =
                 this.handlerConfigurationService.AddHandlerConfigurationAsync(nullHandlerConfiguration);
+
+            HandlerConfigurationValidationException actualHandlerConfigurationValidationException =
+                await Assert.ThrowsAsync<HandlerConfigurationValidationException>(
+                    addHandlerConfigurationTask.AsTask);
+
+            // then
+            actualHandlerConfigurationValidationException.Should().BeEquivalentTo(
+                expectedHandlerConfigurationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedHandlerConfigurationValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHandlerConfigurationAsync(It.IsAny<HandlerConfiguration>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfHandlerConfigurationIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidHandlerConfiguration = new HandlerConfiguration
+            {
+                Id = Guid.Empty,
+                Name = invalidText,
+                Value = invalidText
+            };
+
+            var invalidHandlerConfigurationException =
+                new InvalidHandlerConfigurationException(
+                    message: "Handler configuration is invalid, fix the errors and try again.");
+
+            invalidHandlerConfigurationException.AddData(
+                key: nameof(HandlerConfiguration.Id),
+                values: "Required");
+
+            invalidHandlerConfigurationException.AddData(
+                key: nameof(HandlerConfiguration.Name),
+                values: "Required");
+
+            invalidHandlerConfigurationException.AddData(
+                key: nameof(HandlerConfiguration.Value),
+                values: "Required");
+
+            var expectedHandlerConfigurationValidationException =
+                new HandlerConfigurationValidationException(
+                    message: "Handler configuration validation error occurred, fix the errors and try again.",
+                    innerException: invalidHandlerConfigurationException);
+
+            // when
+            ValueTask<HandlerConfiguration> addHandlerConfigurationTask =
+                this.handlerConfigurationService.AddHandlerConfigurationAsync(invalidHandlerConfiguration);
 
             HandlerConfigurationValidationException actualHandlerConfigurationValidationException =
                 await Assert.ThrowsAsync<HandlerConfigurationValidationException>(
