05e49bb3cf6c580b8e9575185f5524e11fe0fe7a
Christo du Toit <christo@dutoit.co.uk>
2026-04-19 22:32:17 +0100

    ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/HandlerConfigurations/HandlerConfigurationServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/HandlerConfigurations/HandlerConfigurationServiceTests.Exceptions.Add.cs
@@ -8,6 +8,7 @@ using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations;
 using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 
@@ -159,6 +160,57 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.HandlerConfiguration
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedHandlerConfigurationDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHandlerConfigurationAsync(It.IsAny<HandlerConfiguration>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            HandlerConfiguration someHandlerConfiguration = CreateRandomHandlerConfiguration();
+            var dbUpdateException = new DbUpdateException();
+
+            var failedHandlerConfigurationStorageException =
+                new FailedHandlerConfigurationStorageException(
+                    message: "Failed handler configuration storage error occurred, contact support.",
+                    innerException: dbUpdateException);
+
+            var expectedHandlerConfigurationDependencyException =
+                new HandlerConfigurationDependencyException(
+                    message: "Handler configuration dependency error occurred, contact support.",
+                    innerException: failedHandlerConfigurationStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateException);
+
+            // when
+            ValueTask<HandlerConfiguration> addHandlerConfigurationTask =
+                this.handlerConfigurationService.AddHandlerConfigurationAsync(someHandlerConfiguration);
+
+            HandlerConfigurationDependencyException actualHandlerConfigurationDependencyException =
+                await Assert.ThrowsAsync<HandlerConfigurationDependencyException>(
+                    addHandlerConfigurationTask.AsTask);
+
+            // then
+            actualHandlerConfigurationDependencyException.Should()
+                .BeEquivalentTo(expectedHandlerConfigurationDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedHandlerConfigurationDependencyException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
