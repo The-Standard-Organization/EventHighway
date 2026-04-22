c88519e7a0b0889e24d7e1ed962cf8a5abac8a03
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-04-22 19:55:06 +0100

    CODERUB: Code cleanup

--- EventHighway.Core/Services/Foundations/EventArchives/V1/IEventV1ArchiveService.cs
+++ EventHighway.Core/Services/Foundations/EventArchives/V1/IEventV1ArchiveService.cs
@@ -11,13 +11,9 @@ namespace EventHighway.Core.Services.Foundations.EventArchives.V1
 {
     public partial interface IEventV1ArchiveService
     {
-        ValueTask<EventV1Archive> AddEventV1ArchiveAsync(
-            EventV1Archive eventV1Archive);
-
+        ValueTask<EventV1Archive> AddEventV1ArchiveAsync(EventV1Archive eventV1Archive);
         ValueTask<IQueryable<EventV1Archive>> RetrieveAllEventV1ArchivesAsync();
-
-        ValueTask<EventV1Archive> RemoveEventV1ArchiveByIdAsync(
-            Guid eventArchiveV1Id);
+        ValueTask<EventV1Archive> RemoveEventV1ArchiveByIdAsync(Guid eventArchiveV1Id);
 
     }
 }
