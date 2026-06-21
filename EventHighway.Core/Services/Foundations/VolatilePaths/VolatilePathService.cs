2fb8cef55da61ffa16850018112f7661c829866e
Christo du Toit <christo@dutoit.co.uk>
2026-06-21 21:28:45 +0100

    PASS: VolatilePathService - ShouldRemoveVolatilePathsFromValidJsonContentAsync

--- EventHighway.Core/Services/Foundations/VolatilePaths/VolatilePathService.cs
+++ EventHighway.Core/Services/Foundations/VolatilePaths/VolatilePathService.cs
@@ -2,7 +2,9 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using EventHighway.Core.Brokers.Jsons;
 using EventHighway.Core.Brokers.Loggings;
@@ -24,7 +26,51 @@ namespace EventHighway.Core.Services.Foundations.VolatilePaths
 
         public ValueTask<string> RemoveVolatilePathsAsync(
             string content,
-            string[] volatileContentPaths) =>
-                throw new NotImplementedException();
+            string[] volatileContentPaths)
+        {
+            if (!this.jsonBroker.IsValidJson(content))
+                return ValueTask.FromResult(content);
+
+            foreach (string path in volatileContentPaths)
+                content = this.jsonBroker.RemoveNode(content, path);
+
+            return ValueTask.FromResult(Canonicalize(content));
+        }
+
+        private static string Canonicalize(string json)
+        {
+            JsonNode node = JsonNode.Parse(json);
+            return SerializeSorted(node);
+        }
+
+        private static string SerializeSorted(JsonNode node)
+        {
+            if (node is JsonObject obj)
+            {
+                var sorted = new SortedDictionary<string, JsonNode>();
+
+                foreach (var property in obj)
+                    sorted[property.Key] = property.Value;
+
+                var result = new JsonObject();
+
+                foreach (var kvp in sorted)
+                    result[kvp.Key] = JsonNode.Parse(SerializeSorted(kvp.Value));
+
+                return result.ToJsonString();
+            }
+
+            if (node is JsonArray arr)
+            {
+                var resultArr = new JsonArray();
+
+                foreach (JsonNode item in arr)
+                    resultArr.Add(JsonNode.Parse(SerializeSorted(item)));
+
+                return resultArr.ToJsonString();
+            }
+
+            return node?.ToJsonString() ?? "null";
+        }
     }
 }
