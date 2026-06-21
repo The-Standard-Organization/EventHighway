4bfce832a28d0d4a0d81519cf8db58a4f6106325
Christo du Toit <christo@dutoit.co.uk>
2026-06-21 21:54:05 +0100

    PASS: VolatilePathService - ShouldThrowDependencyValidationExceptionOnRemoveVolatilePathsWhenJsonExceptionOccursAsync

--- EventHighway.Core/Services/Foundations/VolatilePaths/VolatilePathService.Exceptions.cs
+++ EventHighway.Core/Services/Foundations/VolatilePaths/VolatilePathService.Exceptions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Text.Json;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.VolatilePaths.Exceptions;
 using Xeptions;
@@ -22,6 +23,28 @@ namespace EventHighway.Core.Services.Foundations.VolatilePaths
             {
                 throw await CreateAndLogValidationExceptionAsync(invalidVolatilePathServiceException);
             }
+            catch (JsonException jsonException)
+            {
+                var failedJsonVolatilePathServiceException =
+                    new FailedJsonVolatilePathServiceException(
+                        message: "Failed jsonvolatile path service error occurred, contact support.",
+                        innerException: jsonException);
+
+                throw await CreateAndLogDependencyValidationExceptionAsync(failedJsonVolatilePathServiceException);
+            }
+        }
+
+        private async ValueTask<VolatilePathServiceDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var volatilePathServiceDependencyValidationException =
+                new VolatilePathServiceDependencyValidationException(
+                    message: "Volatile path service dependency validation error occurred, fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(volatilePathServiceDependencyValidationException);
+
+            return volatilePathServiceDependencyValidationException;
         }
 
         private async ValueTask<VolatilePathServiceValidationException> CreateAndLogValidationExceptionAsync(
