83648ea4fcd5ff46077a6aea6f314ae8cefce243
Christo du Toit <christo@dutoit.co.uk>
2026-06-21 23:54:30 +0100

    [FAIL] EventV2ProcessingService should throw dependency exception on remove volatile paths if dependency exception occurs
    Co-Authored-By: Christo du Toit <christo@dutoit.co.uk>

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.RemoveVolatilePaths.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.RemoveVolatilePaths.cs
@@ -67,5 +67,59 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task
+            ShouldThrowDependencyExceptionOnRemoveVolatilePathsIfDependencyExceptionOccursAndLogItAsync(
+                Xeption dependencyException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventV2 someEventV2 = CreateRandomEventV2();
+
+            var expectedEventV2ProcessingDependencyException =
+                new EventV2ProcessingDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RemoveVolatilePathsAsync(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<string> removeVolatilePathsTask =
+                this.eventV2ProcessingService.RemoveVolatilePathsAsync(
+                    someEventV2,
+                    randomCancellationToken);
+
+            EventV2ProcessingDependencyException
+                actualEventV2ProcessingDependencyException =
+                    await Assert.ThrowsAsync<EventV2ProcessingDependencyException>(
+                        removeVolatilePathsTask.AsTask);
+
+            // then
+            actualEventV2ProcessingDependencyException.Should()
+                .BeEquivalentTo(expectedEventV2ProcessingDependencyException);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RemoveVolatilePathsAsync(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
