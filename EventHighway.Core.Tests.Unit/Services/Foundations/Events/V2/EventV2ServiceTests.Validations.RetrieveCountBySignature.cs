eb1467cd93058a4cf73c092866dc64557f3bfc41
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 01:01:43 +0100

    FAIL: EventV2Service - ShouldThrowValidationExceptionOnRetrieveCountBySignatureIfFieldsAreInvalidAndLogItAsync

--- EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Validations.RetrieveCountBySignature.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Validations.RetrieveCountBySignature.cs
@@ -2,8 +2,10 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Configurations.LoopDetections;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
 using FluentAssertions;
@@ -48,6 +50,80 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedEventV2ValidationException))),
                         Times.Once);
+
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveCountBySignatureIfFieldsAreInvalidAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventV2 invalidEventV2 = new EventV2
+            {
+                EventAddressId = Guid.Empty,
+                EventName = string.Empty,
+                ContentHash = string.Empty
+            };
+
+            LoopDetection nullLoopDetectionConfig = null;
+
+            var invalidEventV2Exception =
+                new InvalidEventV2Exception(
+                    message: "Event is invalid, fix the errors and try again.");
+
+            invalidEventV2Exception.UpsertDataList(
+                key: nameof(EventV2.EventAddressId),
+                value: "Required");
+
+            invalidEventV2Exception.UpsertDataList(
+                key: nameof(EventV2.EventName),
+                value: "Required");
+
+            invalidEventV2Exception.UpsertDataList(
+                key: nameof(EventV2.ContentHash),
+                value: "Required");
+
+            invalidEventV2Exception.UpsertDataList(
+                key: nameof(LoopDetection.Window),
+                value: "Required");
+
+            var expectedEventV2ValidationException =
+                new EventV2ValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventV2Exception);
+
+            this.configurationBrokerMock
+                .Setup(broker => broker.GetLoopDetectionConfiguration())
+                    .Returns(nullLoopDetectionConfig);
+
+            // when
+            ValueTask<int> retrieveCountTask =
+                this.eventV2Service.RetrieveEventV2CountBySignatureAsync(
+                    invalidEventV2,
+                    randomCancellationToken);
+
+            EventV2ValidationException actualEventV2ValidationException =
+                await Assert.ThrowsAsync<EventV2ValidationException>(
+                    retrieveCountTask.AsTask);
+
+            // then
+            actualEventV2ValidationException.Should().BeEquivalentTo(
+                expectedEventV2ValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ValidationException))),
+                        Times.Once);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetLoopDetectionConfiguration(),
+                    Times.Once);
 
             this.configurationBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
