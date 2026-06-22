0012f55bf07dc12d83d0e3d0ece62a9fec1a49ba
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 01:20:21 +0100

    FAIL: EventV2Service - ShouldThrowDependencyExceptionOnRetrieveCountBySignatureIfSqlExceptionOccursAndLogItAsync

--- EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Exceptions.RetrieveCountBySignature.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Exceptions.RetrieveCountBySignature.cs
@@ -9,6 +9,7 @@ using EventHighway.Core.Models.Configurations.LoopDetections;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Moq;
 
 namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
@@ -121,6 +122,72 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
             this.configurationBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveCountBySignatureIfSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            EventV2 someEventV2 =
+                CreateRandomEventV2(dates: GetRandomDateTimeOffset());
+
+            var loopDetectionConfig = new LoopDetection
+            {
+                Window = TimeSpan.FromSeconds(GetRandomNumber())
+            };
+
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageEventV2Exception =
+                new FailedStorageEventV2Exception(
+                    message: "Failed event storage error occurred, contact support.",
+                    innerException: sqlException,
+                    data: sqlException.Data);
+
+            var expectedEventV2DependencyException =
+                new EventV2DependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: failedStorageEventV2Exception);
+
+            this.configurationBrokerMock
+                .Setup(broker => broker.GetLoopDetectionConfiguration())
+                    .Returns(loopDetectionConfig);
+
+            this.storageBrokerMock
+                .Setup(broker => broker.SelectAllEventV2sAsync(
+                    TestContext.Current.CancellationToken))
+                        .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<int> retrieveCountTask =
+                this.eventV2Service.RetrieveEventV2CountBySignatureAsync(
+                    someEventV2,
+                    TestContext.Current.CancellationToken);
+
+            EventV2DependencyException actualEventV2DependencyException =
+                await Assert.ThrowsAsync<EventV2DependencyException>(
+                    retrieveCountTask.AsTask);
+
+            // then
+            actualEventV2DependencyException.Should().BeEquivalentTo(
+                expectedEventV2DependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2DependencyException))),
+                        Times.Once);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetLoopDetectionConfiguration(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllEventV2sAsync(TestContext.Current.CancellationToken),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
