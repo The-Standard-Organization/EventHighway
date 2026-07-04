43cd3c8002656bdc0818af5ae9d24972dd19459b
Christo du Toit <christo@dutoit.co.uk>
2026-07-04 23:05:41 +0100

    ShouldRetrieveBatchOfRetryListenerEventV2sWithTakeAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingServiceTests.Logic.RetrieveBatchRetry.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingServiceTests.Logic.RetrieveBatchRetry.cs
@@ -51,5 +51,45 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEvents.V2
             this.listenerEventV2ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRetrieveBatchOfRetryListenerEventV2sWithTakeAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int randomTake = GetRandomNumber();
+            int inputTake = randomTake;
+
+            IEnumerable<ListenerEventV2> retrievedListenerEventV2s =
+                CreateRandomListenerEventV2s();
+
+            IEnumerable<ListenerEventV2> expectedListenerEventV2s =
+                retrievedListenerEventV2s;
+
+            this.listenerEventV2ServiceMock.Setup(service =>
+                service.RetrieveRetryBatchListenerEventV2sWithEventWithEventListenerAsync(
+                    inputTake,
+                    randomCancellationToken))
+                        .ReturnsAsync(retrievedListenerEventV2s);
+
+            // when
+            IEnumerable<ListenerEventV2> actualListenerEventV2s =
+                await this.listenerEventV2ProcessingService
+                    .RetrieveBatchOfRetryListenerEventV2sAsync(inputTake, randomCancellationToken);
+
+            // then
+            actualListenerEventV2s.Should().BeEquivalentTo(expectedListenerEventV2s);
+
+            this.listenerEventV2ServiceMock.Verify(service =>
+                service.RetrieveRetryBatchListenerEventV2sWithEventWithEventListenerAsync(
+                    inputTake,
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.listenerEventV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
