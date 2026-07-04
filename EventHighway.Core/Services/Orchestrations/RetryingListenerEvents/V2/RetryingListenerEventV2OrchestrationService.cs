2d47c8ca0eebe80929118f51f4d026071326a57b
Christo du Toit <christo@dutoit.co.uk>
2026-07-04 21:31:04 +0100

    ShouldRetryListenerEventV2SuccessfullyAsync -> PASS

--- EventHighway.Core/Services/Orchestrations/RetryingListenerEvents/V2/RetryingListenerEventV2OrchestrationService.cs
+++ EventHighway.Core/Services/Orchestrations/RetryingListenerEvents/V2/RetryingListenerEventV2OrchestrationService.cs
@@ -3,12 +3,15 @@
 // ----------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using EventHighway.Core.Brokers.Configurations;
 using EventHighway.Core.Brokers.Loggings;
 using EventHighway.Core.Brokers.Times;
+using EventHighway.Core.Models.Services.Foundations.EventCall.V2;
 using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using EventHighway.Core.Models.Services.Foundations.PromotedProperties;
 using EventHighway.Core.Services.Processings.EventCalls.V2;
 using EventHighway.Core.Services.Processings.ListenerEvents.V2;
 
@@ -37,9 +40,56 @@ namespace EventHighway.Core.Services.Orchestrations.RetryingListenerEvents.V2
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<ListenerEventV2> RetryListenerEventV2Async(
+        public async ValueTask<ListenerEventV2> RetryListenerEventV2Async(
             ListenerEventV2 listenerEventV2,
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+            CancellationToken cancellationToken = default)
+        {
+            IEnumerable<string> requiredKeys =
+                string.IsNullOrWhiteSpace(listenerEventV2.EventListenerV2.PromotedProperties)
+                    ? Array.Empty<string>()
+                    : await this.eventCallV2ProcessingService
+                        .SplitPromotedPropertyKeysAsync(
+                            listenerEventV2.EventListenerV2.PromotedProperties,
+                            cancellationToken);
+
+            var eventCallV2 = new EventCallV2
+            {
+                Content = listenerEventV2.EventV2.Content,
+                HandlerId = listenerEventV2.EventListenerV2.HandlerId,
+                HandlerName = listenerEventV2.EventListenerV2.HandlerName,
+                FilterCriteria = listenerEventV2.EventListenerV2.FilterCriteria,
+                RequiredPromotedProperties = requiredKeys,
+                Response = null
+            };
+
+            eventCallV2.PromotedProperties =
+                string.IsNullOrWhiteSpace(listenerEventV2.EventV2.Content)
+                    || string.IsNullOrWhiteSpace(listenerEventV2.EventListenerV2.PromotedProperties)
+                ? new List<PromotedProperty>()
+                : await this.eventCallV2ProcessingService
+                    .PromotePropertiesAsync(
+                        listenerEventV2.EventV2.Content,
+                        listenerEventV2.EventListenerV2.PromotedProperties,
+                        cancellationToken);
+
+            EventCallV2 ranEventCallV2 =
+                await this.eventCallV2ProcessingService
+                    .RunEventCallV2Async(eventCallV2, cancellationToken);
+
+            listenerEventV2.Response = ranEventCallV2.Response;
+            listenerEventV2.ResponseCode = ranEventCallV2.ResponseCode;
+            listenerEventV2.ResponseMessage = ranEventCallV2.ResponseMessage;
+
+            DateTimeOffset now =
+                await this.dateTimeBroker.GetDateTimeOffsetAsync();
+
+            listenerEventV2.DispatchedDate = now;
+            listenerEventV2.Status = ListenerEventStatusV2.Success;
+            listenerEventV2.NextRetryAttemptNotBefore = null;
+            listenerEventV2.UpdatedDate = now;
+
+            return await this.listenerEventV2ProcessingService
+                .ModifyListenerEventV2Async(listenerEventV2, cancellationToken);
+        }
     }
 }
