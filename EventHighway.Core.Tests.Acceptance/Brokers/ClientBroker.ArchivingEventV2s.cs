b145bd4c1ce3784616606565dad01448ce7feda8
Christo du Toit <christo@dutoit.co.uk>
2026-07-05 23:28:15 +0100

    ShouldPurgeEventArchiveV2sFromConfigurationAsync -> FAIL

--- EventHighway.Core.Tests.Acceptance/Brokers/ClientBroker.ArchivingEventV2s.cs
+++ EventHighway.Core.Tests.Acceptance/Brokers/ClientBroker.ArchivingEventV2s.cs
@@ -15,5 +15,8 @@ namespace EventHighway.Core.Tests.Acceptance.Brokers
         public async ValueTask PurgeEventArchiveV2sAsync(DateTimeOffset olderThan) =>
             await this.eventHighwayClient.V2.ArchivingEventV2Client
                 .PurgeEventArchiveV2sAsync(olderThan);
+
+        public ValueTask PurgeEventArchiveV2sAsync() =>
+            throw new NotImplementedException();
     }
 }
