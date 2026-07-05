b7547a1052e7481f546502079f86b54a4813b2a9
Christo du Toit <christo@dutoit.co.uk>
2026-07-05 02:32:35 +0100

    ShouldThrowDependencyExceptionOnRetryFailedListenerEventV2sIfDependencyErrorOccursAndLogItAsync -> PASS
    Map EventCall/ListenerEvent processing dependency and service errors to a RetryingListenerEventV2OrchestrationDependencyException in the void TryCatch overload.

--- EventHighway.Core/Services/Orchestrations/RetryingListenerEvents/V2/RetryingListenerEventV2OrchestrationService.Exceptions.cs
+++ EventHighway.Core/Services/Orchestrations/RetryingListenerEvents/V2/RetryingListenerEventV2OrchestrationService.Exceptions.cs
@@ -148,6 +148,30 @@ namespace EventHighway.Core.Services.Orchestrations.RetryingListenerEvents.V2
                 throw await CreateAndLogDependencyValidationExceptionAsync(
                     listenerEventV2ProcessingDependencyValidationException);
             }
+            catch (EventCallV2ProcessingDependencyException
+                eventCallV2ProcessingDependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(
+                    eventCallV2ProcessingDependencyException);
+            }
+            catch (EventCallV2ProcessingServiceException
+                eventCallV2ProcessingServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(
+                    eventCallV2ProcessingServiceException);
+            }
+            catch (ListenerEventV2ProcessingDependencyException
+                listenerEventV2ProcessingDependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(
+                    listenerEventV2ProcessingDependencyException);
+            }
+            catch (ListenerEventV2ProcessingServiceException
+                listenerEventV2ProcessingServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(
+                    listenerEventV2ProcessingServiceException);
+            }
             catch (Exception exception)
             {
                 var failedRetryingListenerEventV2OrchestrationServiceException =
