e5813867e705b056d48f414894ba7a332003229c
Christo du Toit <christo@dutoit.co.uk>
2026-07-06 21:53:16 +0100

    ShouldThrowDependencyExceptionOnRetrieveInfrastructureHealthV2IfDependencyExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core/Services/Orchestrations/HealthInfrastructures/V2/HealthInfrastructureV2OrchestrationService.Exceptions.cs
+++ EventHighway.Core/Services/Orchestrations/HealthInfrastructures/V2/HealthInfrastructureV2OrchestrationService.Exceptions.cs
@@ -5,6 +5,9 @@
 using System;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventListeners.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2.Exceptions;
 using EventHighway.Core.Models.Services.Orchestrations.HealthInfrastructures.V2.Exceptions;
 using Xeptions;
 
@@ -40,6 +43,30 @@ namespace EventHighway.Core.Services.Orchestrations.HealthInfrastructures.V2
             {
                 throw;
             }
+            catch (EventAddressV2DependencyException eventAddressV2DependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventAddressV2DependencyException);
+            }
+            catch (EventAddressV2ServiceException eventAddressV2ServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventAddressV2ServiceException);
+            }
+            catch (EventListenerV2DependencyException eventListenerV2DependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventListenerV2DependencyException);
+            }
+            catch (EventListenerV2ServiceException eventListenerV2ServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventListenerV2ServiceException);
+            }
+            catch (EventParticipantV2DependencyException eventParticipantV2DependencyException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventParticipantV2DependencyException);
+            }
+            catch (EventParticipantV2ServiceException eventParticipantV2ServiceException)
+            {
+                throw await CreateAndLogDependencyExceptionAsync(eventParticipantV2ServiceException);
+            }
         }
 
         private async ValueTask<HealthInfrastructureV2OrchestrationDependencyException>
@@ -49,6 +76,20 @@ namespace EventHighway.Core.Services.Orchestrations.HealthInfrastructures.V2
                 new HealthInfrastructureV2OrchestrationDependencyException(
                     message: "Health infrastructure dependency error occurred, contact support.",
                     innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(
+                healthInfrastructureV2OrchestrationDependencyException);
+
+            return healthInfrastructureV2OrchestrationDependencyException;
+        }
+
+        private async ValueTask<HealthInfrastructureV2OrchestrationDependencyException>
+            CreateAndLogDependencyExceptionAsync(Xeption exception)
+        {
+            var healthInfrastructureV2OrchestrationDependencyException =
+                new HealthInfrastructureV2OrchestrationDependencyException(
+                    message: "Health infrastructure dependency error occurred, contact support.",
+                    innerException: exception.InnerException as Xeption);
 
             await this.loggingBroker.LogErrorAsync(
                 healthInfrastructureV2OrchestrationDependencyException);
