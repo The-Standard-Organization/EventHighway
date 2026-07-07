bddb7949ae8cc19617df39f22bd4e3e3693da183
Christo du Toit <christo@dutoit.co.uk>
2026-07-07 17:49:00 +0100

    ShouldRetrieveRetryHealthV2Async -> FAIL

--- EventHighway.Core/Clients/HealthChecks/V2/HealthRetry/HealthRetryClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthRetry/HealthRetryClientV2.cs
@@ -5,11 +5,8 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using EventHighway.Core.Models.Clients.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
-using EventHighway.Core.Models.Services.Orchestrations.RetrySummaries.V2.Exceptions;
-using EventHighway.Core.Services.Orchestrations.RetrySummaries.V2;
-using Xeptions;
+using EventHighway.Core.Services.Coordinations.HealthChecks.V2;
 
 namespace EventHighway.Core.Clients.HealthChecks.V2
 {
@@ -19,71 +16,21 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
     /// </summary>
     internal class HealthRetryClientV2 : IHealthRetryClientV2
     {
-        private readonly IRetrySummaryV2OrchestrationService retrySummaryV2OrchestrationService;
+        private readonly IHealthV2CoordinationService healthV2CoordinationService;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HealthRetryClientV2"/> class with the
-        /// specified retry summary orchestration service.
+        /// specified health coordination service.
         /// </summary>
-        /// <param name="retrySummaryV2OrchestrationService">The orchestration service for
-        /// retry-health summaries.</param>
-        public HealthRetryClientV2(IRetrySummaryV2OrchestrationService retrySummaryV2OrchestrationService) =>
-            this.retrySummaryV2OrchestrationService = retrySummaryV2OrchestrationService;
+        /// <param name="healthV2CoordinationService">The coordination service for health
+        /// reports.</param>
+        public HealthRetryClientV2(IHealthV2CoordinationService healthV2CoordinationService) =>
+            this.healthV2CoordinationService = healthV2CoordinationService;
 
-        /// <summary>
-        /// Retrieves the current retry-health summary asynchronously by delegating to the
-        /// coordination service and handling any exceptions that occur.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to allow cancellation of the
-        /// asynchronous operation.</param>
-        /// <returns>A <see cref="ValueTask{RetryHealthSummaryV2}"/> representing the asynchronous
-        /// operation that returns the retry-health summary.</returns>
-        public async ValueTask<RetryHealthSummaryV2> RetrieveRetryHealthV2Async(
-            CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                return await this.retrySummaryV2OrchestrationService
-                    .RetrieveRetryHealthV2Async(cancellationToken);
-            }
-            catch (RetrySummaryV2OrchestrationDependencyException
-                retrySummaryV2OrchestrationDependencyException)
-            {
-                throw CreateHealthRetryClientV2DependencyException(
-                    retrySummaryV2OrchestrationDependencyException.InnerException as Xeption);
-            }
-            catch (RetrySummaryV2OrchestrationServiceException
-                retrySummaryV2OrchestrationServiceException)
-            {
-                throw CreateHealthRetryClientV2DependencyException(
-                    retrySummaryV2OrchestrationServiceException.InnerException as Xeption);
-            }
-            catch (OperationCanceledException)
-            {
-                throw;
-            }
-            catch (Exception exception)
-            {
-                throw CreateHealthRetryClientV2ServiceException(exception as Xeption);
-            }
-        }
-
-        private static HealthRetryClientV2DependencyException
-            CreateHealthRetryClientV2DependencyException(Xeption innerException)
-        {
-            return new HealthRetryClientV2DependencyException(
-                message: "Health client dependency error occurred, contact support.",
-                innerException: innerException,
-                data: innerException?.Data);
-        }
-
-        private static HealthRetryClientV2ServiceException
-            CreateHealthRetryClientV2ServiceException(Xeption innerException)
-        {
-            return new HealthRetryClientV2ServiceException(
-                message: "Health client service error occurred, contact support.",
-                innerException: innerException,
-                data: innerException?.Data);
-        }
+        public ValueTask<RetryHealthSummaryV2> RetrieveRetryHealthV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default) =>
+            throw new NotImplementedException();
     }
 }
