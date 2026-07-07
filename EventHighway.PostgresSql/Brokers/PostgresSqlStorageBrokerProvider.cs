36750de43c837962d131f74e3d98b042d9decc80
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-07 14:12:39 +0100

    PROVIDERS:  Add PostgreSQL provider implementation

---
+++ EventHighway.PostgresSql/Brokers/PostgresSqlStorageBrokerProvider.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using EventHighway.Core.Brokers.Storages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EventHighway.PostgresSql.Brokers
+{
+    public sealed class PostgresSqlStorageBrokerProvider : IStorageBrokerProvider
+    {
+        private readonly string connectionString;
+
+        public PostgresSqlStorageBrokerProvider(string connectionString) =>
+            this.connectionString = connectionString;
+
+        public void Configure(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseNpgsql(
+                this.connectionString,
+                npgsqlOptions => npgsqlOptions.MigrationsAssembly("EventHighway.PostgresSql"));
+        public void ConfigureModel(ModelBuilder modelBuilder)
+        {
+            foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (IMutableProperty property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTimeOffset) ||
+                        property.ClrType == typeof(DateTimeOffset?))
+                    {
+                        property.SetColumnType("timestamptz");
+                        property.SetPrecision(6);
+                    }
+                }
+            }
+        }
+    }
+}
