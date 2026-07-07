77d618fbade3d1af0d29da6e405617f84be3870d
Christo du Toit <christo@dutoit.co.uk>
2026-07-07 17:47:13 +0100

    ShouldThrowServiceExceptionOnRetrieveDuplicateDetectionSummaryIfUnexpectedErrorOccursAsync -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthDuplicate/HealthDuplicateClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthDuplicate/HealthDuplicateClientV2.cs
@@ -5,8 +5,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Clients.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Services.Coordinations.HealthChecks.V2;
+using Xeptions;
 
 namespace EventHighway.Core.Clients.HealthChecks.V2
 {
@@ -32,10 +35,72 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
             DateTimeOffset windowStart,
             CancellationToken cancellationToken = default)
         {
-            HealthReportV2 healthReport = await this.healthV2CoordinationService
-                .RetrieveDuplicateReportV2Async(period, windowStart, cancellationToken);
+            try
+            {
+                HealthReportV2 healthReport = await this.healthV2CoordinationService
+                    .RetrieveDuplicateReportV2Async(period, windowStart, cancellationToken);
 
-            return healthReport.Duplicates;
+                return healthReport.Duplicates;
+            }
+            catch (HealthV2CoordinationValidationException
+                healthV2CoordinationValidationException)
+            {
+                throw CreateHealthDuplicateClientV2ValidationException(
+                    healthV2CoordinationValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyValidationException
+                healthV2CoordinationDependencyValidationException)
+            {
+                throw CreateHealthDuplicateClientV2ValidationException(
+                    healthV2CoordinationDependencyValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyException
+                healthV2CoordinationDependencyException)
+            {
+                throw CreateHealthDuplicateClientV2DependencyException(
+                    healthV2CoordinationDependencyException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationServiceException
+                healthV2CoordinationServiceException)
+            {
+                throw CreateHealthDuplicateClientV2DependencyException(
+                    healthV2CoordinationServiceException.InnerException as Xeption);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (Exception exception)
+            {
+                throw CreateHealthDuplicateClientV2ServiceException(exception as Xeption);
+            }
+        }
+
+        private static HealthDuplicateClientV2ValidationException
+            CreateHealthDuplicateClientV2ValidationException(Xeption innerException)
+        {
+            return new HealthDuplicateClientV2ValidationException(
+                message: "Health client validation error occurred, fix the errors and try again.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthDuplicateClientV2DependencyException
+            CreateHealthDuplicateClientV2DependencyException(Xeption innerException)
+        {
+            return new HealthDuplicateClientV2DependencyException(
+                message: "Health client dependency error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthDuplicateClientV2ServiceException
+            CreateHealthDuplicateClientV2ServiceException(Xeption innerException)
+        {
+            return new HealthDuplicateClientV2ServiceException(
+                message: "Health client service error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
         }
     }
 }
