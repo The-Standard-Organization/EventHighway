3fd5774d2d3b644519ccf63293b923c2b756b922
Christo du Toit <christo@dutoit.co.uk>
2026-07-07 16:36:39 +0100

    ShouldRetrieveHealthRagStatusV2Async -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthStatus/HealthStatusClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthStatus/HealthStatusClientV2.cs
@@ -28,10 +28,15 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
         public HealthStatusClientV2(IHealthV2CoordinationService healthV2CoordinationService) =>
             this.healthV2CoordinationService = healthV2CoordinationService;
 
-        public ValueTask<IReadOnlyList<HealthCheckItemV2>> RetrieveHealthRagStatusV2Async(
+        public async ValueTask<IReadOnlyList<HealthCheckItemV2>> RetrieveHealthRagStatusV2Async(
             TrafficPeriodV2 period,
             DateTimeOffset windowStart,
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+            CancellationToken cancellationToken = default)
+        {
+            HealthReportV2 healthReport = await this.healthV2CoordinationService
+                .RetrieveHealthCheckItemsReportV2Async(period, windowStart, cancellationToken);
+
+            return healthReport.HealthCheckItems;
+        }
     }
 }
