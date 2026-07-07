52871c2d447329cc95120c5386ca829cafcdaf5d
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-07 15:12:38 +0100

    BROKER: Added provider switch and construct fixture for ClientBroker

--- EventHighway.Core.Tests.Acceptance/Brokers/ClientBroker.cs
+++ EventHighway.Core.Tests.Acceptance/Brokers/ClientBroker.cs
@@ -4,8 +4,11 @@
 
 using System;
 using EventHighway.Abstractions.EventHandlers;
+using EventHighway.Core.Brokers.Storages;
 using EventHighway.Core.Clients.EventHighways;
+using EventHighway.PostgresSql.Brokers;
 using EventHighway.SqlServer.Brokers;
+using Microsoft.Extensions.Configuration;
 
 namespace EventHighway.Core.Tests.Acceptance.Brokers
 {
@@ -15,12 +18,28 @@ namespace EventHighway.Core.Tests.Acceptance.Brokers
 
         public ClientBroker()
         {
-            string connectionString = String.Concat(
-                "Server=(localdb)\\MSSQLLocalDB;Database=EventHighwayDb.Acceptance;",
-                "Trusted_Connection=True;MultipleActiveResultSets=true;Pooling=false");
 
-            SqlServerStorageBrokerProvider storageProvider =
-                  new SqlServerStorageBrokerProvider(connectionString);
+            IConfiguration configuration =
+            new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            string provider = configuration["Database:CurrentProvider"];
+
+            string connectionString =
+                configuration[$"Database:Providers:{provider}:ConnectionString"];
+
+            IStorageBrokerProvider storageProvider =
+                provider switch
+                {
+                    "postgres" =>
+                        new PostgresSqlStorageBrokerProvider(connectionString),
+
+                    _ =>
+                        new SqlServerStorageBrokerProvider(connectionString)
+                };
 
             this.eventHighwayClient = new EventHighwayClient(storageProvider);
         }
