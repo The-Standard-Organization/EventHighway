d7d784b1494abef1a6978f0e04e3374fb119c805
Christo du Toit <christo@dutoit.co.uk>
2026-07-07 17:42:32 +0100

    ShouldThrowServiceExceptionOnRetrieveLoopDetectionSummaryIfUnexpectedErrorOccursAsync -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthLoop/HealthLoopClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthLoop/HealthLoopClientV2.cs
@@ -5,8 +5,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Clients.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Services.Coordinations.HealthChecks.V2;
+using Xeptions;
 
 namespace EventHighway.Core.Clients.HealthChecks.V2
 {
@@ -32,10 +35,72 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
             DateTimeOffset windowStart,
             CancellationToken cancellationToken = default)
         {
-            HealthReportV2 healthReport = await this.healthV2CoordinationService
-                .RetrieveLoopDetectionReportV2Async(period, windowStart, cancellationToken);
+            try
+            {
+                HealthReportV2 healthReport = await this.healthV2CoordinationService
+                    .RetrieveLoopDetectionReportV2Async(period, windowStart, cancellationToken);
 
-            return healthReport.LoopDetection;
+                return healthReport.LoopDetection;
+            }
+            catch (HealthV2CoordinationValidationException
+                healthV2CoordinationValidationException)
+            {
+                throw CreateHealthLoopClientV2ValidationException(
+                    healthV2CoordinationValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyValidationException
+                healthV2CoordinationDependencyValidationException)
+            {
+                throw CreateHealthLoopClientV2ValidationException(
+                    healthV2CoordinationDependencyValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyException
+                healthV2CoordinationDependencyException)
+            {
+                throw CreateHealthLoopClientV2DependencyException(
+                    healthV2CoordinationDependencyException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationServiceException
+                healthV2CoordinationServiceException)
+            {
+                throw CreateHealthLoopClientV2DependencyException(
+                    healthV2CoordinationServiceException.InnerException as Xeption);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (Exception exception)
+            {
+                throw CreateHealthLoopClientV2ServiceException(exception as Xeption);
+            }
+        }
+
+        private static HealthLoopClientV2ValidationException
+            CreateHealthLoopClientV2ValidationException(Xeption innerException)
+        {
+            return new HealthLoopClientV2ValidationException(
+                message: "Health client validation error occurred, fix the errors and try again.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthLoopClientV2DependencyException
+            CreateHealthLoopClientV2DependencyException(Xeption innerException)
+        {
+            return new HealthLoopClientV2DependencyException(
+                message: "Health client dependency error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthLoopClientV2ServiceException
+            CreateHealthLoopClientV2ServiceException(Xeption innerException)
+        {
+            return new HealthLoopClientV2ServiceException(
+                message: "Health client service error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
         }
     }
 }
