6fe1018c4ce526479389215c149b831ed5e5e707
Christo du Toit <christo@dutoit.co.uk>
2026-07-07 17:18:48 +0100

    ShouldThrowServiceExceptionOnRetrieveParticipantSummaryIfUnexpectedErrorOccursAsync -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthParticipant/HealthParticipantClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthParticipant/HealthParticipantClientV2.cs
@@ -6,8 +6,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Clients.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2.Exceptions;
 using EventHighway.Core.Services.Coordinations.HealthChecks.V2;
+using Xeptions;
 
 namespace EventHighway.Core.Clients.HealthChecks.V2
 {
@@ -33,10 +36,72 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
             DateTimeOffset windowStart,
             CancellationToken cancellationToken = default)
         {
-            HealthReportV2 healthReport = await this.healthV2CoordinationService
-                .RetrieveParticipantUsageReportV2Async(period, windowStart, cancellationToken);
+            try
+            {
+                HealthReportV2 healthReport = await this.healthV2CoordinationService
+                    .RetrieveParticipantUsageReportV2Async(period, windowStart, cancellationToken);
 
-            return healthReport.ParticipantUsage;
+                return healthReport.ParticipantUsage;
+            }
+            catch (HealthV2CoordinationValidationException
+                healthV2CoordinationValidationException)
+            {
+                throw CreateHealthParticipantClientV2ValidationException(
+                    healthV2CoordinationValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyValidationException
+                healthV2CoordinationDependencyValidationException)
+            {
+                throw CreateHealthParticipantClientV2ValidationException(
+                    healthV2CoordinationDependencyValidationException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationDependencyException
+                healthV2CoordinationDependencyException)
+            {
+                throw CreateHealthParticipantClientV2DependencyException(
+                    healthV2CoordinationDependencyException.InnerException as Xeption);
+            }
+            catch (HealthV2CoordinationServiceException
+                healthV2CoordinationServiceException)
+            {
+                throw CreateHealthParticipantClientV2DependencyException(
+                    healthV2CoordinationServiceException.InnerException as Xeption);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (Exception exception)
+            {
+                throw CreateHealthParticipantClientV2ServiceException(exception as Xeption);
+            }
+        }
+
+        private static HealthParticipantClientV2ValidationException
+            CreateHealthParticipantClientV2ValidationException(Xeption innerException)
+        {
+            return new HealthParticipantClientV2ValidationException(
+                message: "Health client validation error occurred, fix the errors and try again.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthParticipantClientV2DependencyException
+            CreateHealthParticipantClientV2DependencyException(Xeption innerException)
+        {
+            return new HealthParticipantClientV2DependencyException(
+                message: "Health client dependency error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
+        }
+
+        private static HealthParticipantClientV2ServiceException
+            CreateHealthParticipantClientV2ServiceException(Xeption innerException)
+        {
+            return new HealthParticipantClientV2ServiceException(
+                message: "Health client service error occurred, contact support.",
+                innerException: innerException,
+                data: innerException?.Data);
         }
     }
 }
