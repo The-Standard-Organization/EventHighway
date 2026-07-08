63c9109fb212a8ac70168d1f82b978fe35900495
Christo du Toit <christo@dutoit.co.uk>
2026-07-08 16:03:59 +0100

    ShouldThrowServiceExceptionOnIsUserAssociatedWithParticipantWhenErrorOccursAsync -> FAIL

--- EventHighway.Portal.Web.Tests.Unit/Services/Views/UserEventParticipants/UserEventParticipantsViewServiceTests.Exceptions.cs
+++ EventHighway.Portal.Web.Tests.Unit/Services/Views/UserEventParticipants/UserEventParticipantsViewServiceTests.Exceptions.cs
@@ -61,6 +61,52 @@ namespace EventHighway.Portal.Web.Tests.Unit.Services.Views.UserEventParticipant
         }
 
         [Fact]
+        public async Task ShouldThrowServiceExceptionOnIsUserAssociatedWithParticipantWhenErrorOccursAsync()
+        {
+            // given
+            Guid inputUserId = GetRandomGuid();
+            Guid inputParticipantId = GetRandomGuid();
+            var serviceException = new Exception();
+
+            var failedServiceException =
+                new FailedUserEventParticipantsViewServiceException(
+                    innerException: serviceException);
+
+            var expectedServiceException =
+                new UserEventParticipantsViewServiceException(
+                    innerException: failedServiceException);
+
+            this.userEventParticipantBrokerMock.Setup(broker =>
+                broker.SelectAllUserEventParticipants())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<bool> isAssociatedTask =
+                this.userEventParticipantsViewService.IsUserAssociatedWithParticipantAsync(
+                    inputUserId, inputParticipantId, TestContext.Current.CancellationToken);
+
+            UserEventParticipantsViewServiceException actualException =
+                await Assert.ThrowsAsync<UserEventParticipantsViewServiceException>(
+                    isAssociatedTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedServiceException);
+
+            this.userEventParticipantBrokerMock.Verify(broker =>
+                broker.SelectAllUserEventParticipants(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedServiceException))), Times.Once);
+
+            this.userEventParticipantBrokerMock.VerifyNoOtherCalls();
+            this.identityBrokerMock.VerifyNoOtherCalls();
+            this.eventHighwayBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldThrowServiceExceptionOnRetrieveAssociationsByParticipantIdWhenErrorOccursAsync()
         {
             // given
