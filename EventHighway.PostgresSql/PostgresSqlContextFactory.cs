69a142bd8b059f2aa09e62b1760edad911290cb1
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-09 12:46:08 +0100

    INFRA: Initialize EventHighway.PostgresSql

---
+++ EventHighway.PostgresSql/PostgresSqlContextFactory.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using EventHighway.Core.Brokers.Storages;
+using EventHighway.PostgresSql.Brokers;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EventHighway.PostgresSql
+{
+    internal class PostgresSqlContextFactory : IDesignTimeDbContextFactory<StorageBroker>
+    {
+        public StorageBroker CreateDbContext(string[] args)
+        {
+            string connectionString =
+                "Host=localhost;Port=5432;Database=EventHighwayDB;" +
+                "Username=postgres;Password=postgres";
+
+            return new StorageBroker(new PostgresSqlStorageBrokerProvider(connectionString));
+        }
+    }
+}
