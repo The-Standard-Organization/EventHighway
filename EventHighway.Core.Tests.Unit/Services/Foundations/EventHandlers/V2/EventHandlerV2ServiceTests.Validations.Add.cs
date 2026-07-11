6ed9e6bba5ef31a0fb09c9da176d368e1f83cb19
Christo du Toit <christo@dutoit.co.uk>
2026-07-11 22:21:04 +0100

    ShouldThrowValidationExceptionOnAddIfEventHandlerV2IsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventHandlers/V2/EventHandlerV2ServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventHandlers/V2/EventHandlerV2ServiceTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using EventHighway.Abstractions.EventHandlers;
@@ -37,6 +38,73 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventHandlers.V2
             ValueTask<IEventHandler> addEventHandlerV2Task =
                 this.eventHandlerV2Service.AddEventHandlerV2Async(
                     nullEventHandler, randomCancellationToken);
+
+            EventHandlerV2ValidationException actualEventHandlerV2ValidationException =
+                await Assert.ThrowsAsync<EventHandlerV2ValidationException>(
+                    addEventHandlerV2Task.AsTask);
+
+            // then
+            actualEventHandlerV2ValidationException.Should().BeEquivalentTo(
+                expectedEventHandlerV2ValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is<Xeption>(
+                    actual => actual.SameExceptionAs(
+                        expectedEventHandlerV2ValidationException))),
+                            Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventHandlerV2Async(
+                    It.IsAny<EventHandlerV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.eventHandlerBrokerMock.Verify(broker =>
+                broker.Register(It.IsAny<IEventHandler>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventHandlerBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfEventHandlerV2IsInvalidAndLogItAsync(
+            string invalidName)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var invalidEventHandlerMock = new Mock<IEventHandler>();
+            invalidEventHandlerMock.SetupGet(eventHandler => eventHandler.Id).Returns(Guid.Empty);
+            invalidEventHandlerMock.SetupGet(eventHandler => eventHandler.Name).Returns(invalidName);
+            IEventHandler invalidEventHandler = invalidEventHandlerMock.Object;
+
+            var invalidEventHandlerV2Exception =
+                new InvalidEventHandlerV2Exception(
+                    message: "Event handler is invalid, fix the errors and try again.");
+
+            invalidEventHandlerV2Exception.AddData(
+                key: nameof(IEventHandler.Id),
+                values: "Id required");
+
+            invalidEventHandlerV2Exception.AddData(
+                key: nameof(IEventHandler.Name),
+                values: "Text required");
+
+            var expectedEventHandlerV2ValidationException =
+                new EventHandlerV2ValidationException(
+                    message: "Event handler validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventHandlerV2Exception);
+
+            // when
+            ValueTask<IEventHandler> addEventHandlerV2Task =
+                this.eventHandlerV2Service.AddEventHandlerV2Async(
+                    invalidEventHandler, randomCancellationToken);
 
             EventHandlerV2ValidationException actualEventHandlerV2ValidationException =
                 await Assert.ThrowsAsync<EventHandlerV2ValidationException>(
