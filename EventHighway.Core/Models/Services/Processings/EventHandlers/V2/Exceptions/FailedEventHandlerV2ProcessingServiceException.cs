0a51ed04407c397a073483b8dc25e433f4e46070
Christo du Toit <christo@dutoit.co.uk>
2026-07-11 23:08:08 +0100

    CODE RUB: Wrap FailedEventHandlerV2ProcessingServiceException constructor to 120 chars

--- EventHighway.Core/Models/Services/Processings/EventHandlers/V2/Exceptions/FailedEventHandlerV2ProcessingServiceException.cs
+++ EventHighway.Core/Models/Services/Processings/EventHandlers/V2/Exceptions/FailedEventHandlerV2ProcessingServiceException.cs
@@ -10,7 +10,8 @@ namespace EventHighway.Core.Models.Services.Processings.EventHandlers.V2.Excepti
 {
     public class FailedEventHandlerV2ProcessingServiceException : Xeption
     {
-        public FailedEventHandlerV2ProcessingServiceException(string message, Exception innerException, IDictionary data)
+        public FailedEventHandlerV2ProcessingServiceException(
+            string message, Exception innerException, IDictionary data)
             : base(message, innerException, data)
         { }
     }
