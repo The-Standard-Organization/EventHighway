4f5aad745386a064a4e6aa9dd7139d01317f9ca1
Christo du Toit <christo@dutoit.co.uk>
2026-07-11 22:48:19 +0100

    ShouldRegisterEventHandlerV2IfItDoesNotExistAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventHandlers/V2/EventHandlerV2ProcessingServiceTests.Logic.RetrieveOrRegister.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventHandlers/V2/EventHandlerV2ProcessingServiceTests.Logic.RetrieveOrRegister.cs
@@ -48,5 +48,51 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventHandlers.V2
             this.eventHandlerV2ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRegisterEventHandlerV2IfItDoesNotExistAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            IEventHandler randomEventHandler = CreateRandomEventHandler();
+            IEventHandler inputEventHandler = randomEventHandler;
+            IEventHandler registeredEventHandler = inputEventHandler;
+            IEventHandler expectedEventHandler = registeredEventHandler;
+
+            IQueryable<IEventHandler> emptyEventHandlers =
+                Enumerable.Empty<IEventHandler>().AsQueryable();
+
+            this.eventHandlerV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventHandlerV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(emptyEventHandlers);
+
+            this.eventHandlerV2ServiceMock.Setup(service =>
+                service.AddEventHandlerV2Async(
+                    inputEventHandler, randomCancellationToken))
+                        .ReturnsAsync(registeredEventHandler);
+
+            // when
+            IEventHandler actualEventHandler =
+                await this.eventHandlerV2ProcessingService
+                    .RetrieveOrRegisterEventHandlerV2Async(
+                        inputEventHandler, randomCancellationToken);
+
+            // then
+            actualEventHandler.Should().BeEquivalentTo(expectedEventHandler);
+
+            this.eventHandlerV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventHandlerV2sAsync(randomCancellationToken),
+                    Times.Once);
+
+            this.eventHandlerV2ServiceMock.Verify(service =>
+                service.AddEventHandlerV2Async(
+                    inputEventHandler, randomCancellationToken),
+                        Times.Once);
+
+            this.eventHandlerV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
