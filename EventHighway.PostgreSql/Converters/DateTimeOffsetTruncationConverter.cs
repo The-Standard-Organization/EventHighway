4887794eddfa048f90d448cfc46414b83ba090dd
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-12 03:21:42 +0100

    CODERUB: Added DateTimeOffsetTruncationConverter

---
+++ EventHighway.PostgreSql/Converters/DateTimeOffsetTruncationConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EventHighway.PostgreSql.Services.Foundations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace EventHighway.PostgreSql.Converters
+{
+    internal sealed class DateTimeOffsetTruncationConverter : ValueConverter<DateTimeOffset, DateTimeOffset>
+    {
+        public DateTimeOffsetTruncationConverter()
+            : base(
+                dateTimeOffset => DateTimeService.TruncateToMicroseconds(dateTimeOffset),
+                dateTimeOffset => dateTimeOffset)
+        { }
+    }    
+}
