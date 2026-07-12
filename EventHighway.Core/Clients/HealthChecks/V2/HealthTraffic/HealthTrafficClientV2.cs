c4982d196ab2597ab9901f032852fbbdeba6ea70
Christo du Toit <christo@dutoit.co.uk>
2026-07-12 17:49:32 +0100

    ShouldRetrieveTrafficSnapshotV2ForCustomPeriodAsync -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthTraffic/HealthTrafficClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthTraffic/HealthTrafficClientV2.cs
@@ -39,7 +39,7 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
             try
             {
                 HealthReportV2 healthReport = await this.healthV2CoordinationService
-                    .RetrieveTrafficReportV2Async(period, windowStart, cancellationToken: cancellationToken);
+                    .RetrieveTrafficReportV2Async(period, windowStart, windowEnd, cancellationToken);
 
                 return healthReport.Traffic;
             }
