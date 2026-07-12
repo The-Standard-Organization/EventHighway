bc0bd741b18fa6d91faa111bc03ff5c316c71005
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-12 03:21:04 +0100

    FOUNDATIONS:  Added TruncateToMicroseconds

---
+++ EventHighway.PostgreSql/Services/Foundations/DateTimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EventHighway.Core.Brokers.Times;
+
+namespace EventHighway.PostgreSql.Services.Foundations
+{
+    internal partial class DateTimeService
+    {
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public DateTimeService(IDateTimeBroker dateTimeBroker) =>
+            this.dateTimeBroker = dateTimeBroker;
+
+        public static DateTimeOffset TruncateToMicroseconds(DateTimeOffset dateTimeOffset) =>
+            dateTimeOffset.AddTicks(-(dateTimeOffset.Ticks % TimeSpan.TicksPerMicrosecond));
+    }
+}
