7ea5f4024b1a2362fb5ef33a44c62c7109d63585
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-12 03:19:31 +0100

    BROKERS:Added dataTimeBroker

---
+++ EventHighway.PostgreSql/Brokers/Times/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace EventHighway.PostgreSql.Brokers.Times
+{
+    internal class DateTimeBroker : IDateTimeBroker
+    {
+        public async ValueTask<DateTimeOffset> GetDateTimeOffsetAsync() => 
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ EventHighway.PostgreSql/Brokers/Times/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace EventHighway.PostgreSql.Brokers.Times
+{
+    internal interface IDateTimeBroker
+    {
+        ValueTask<DateTimeOffset> GetDateTimeOffsetAsync();
+    }
+}
