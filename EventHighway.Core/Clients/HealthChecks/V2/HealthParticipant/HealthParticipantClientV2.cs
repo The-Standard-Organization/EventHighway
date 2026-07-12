fd120692113f4a041aac463f76beff12e0c048dc
Christo du Toit <christo@dutoit.co.uk>
2026-07-12 17:54:36 +0100

    ShouldRetrieveParticipantSummaryV2ForCustomPeriodAsync -> PASS

--- EventHighway.Core/Clients/HealthChecks/V2/HealthParticipant/HealthParticipantClientV2.cs
+++ EventHighway.Core/Clients/HealthChecks/V2/HealthParticipant/HealthParticipantClientV2.cs
@@ -40,7 +40,7 @@ namespace EventHighway.Core.Clients.HealthChecks.V2
             try
             {
                 HealthReportV2 healthReport = await this.healthV2CoordinationService
-                    .RetrieveParticipantUsageReportV2Async(period, windowStart, cancellationToken: cancellationToken);
+                    .RetrieveParticipantUsageReportV2Async(period, windowStart, windowEnd, cancellationToken);
 
                 return healthReport.ParticipantUsage;
             }
