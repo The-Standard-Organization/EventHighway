241d6982c6f2f9c976311d0c754306dd4a2d126c
Christo du Toit <christo@dutoit.co.uk>
2026-07-12 01:08:49 +0100

    ShouldRetrieveAllEventHandlersAsync -> PASS

--- EventHighway.Portal.Web/Services/Views/EventHandlers/EventHandlersViewService.cs
+++ EventHighway.Portal.Web/Services/Views/EventHandlers/EventHandlersViewService.cs
@@ -2,10 +2,11 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventHandler.V2;
 using EventHighway.Portal.Web.Brokers.EventHighways;
 using EventHighway.Portal.Web.Brokers.Loggings;
 using EventHighway.Portal.Web.Models.Views.EventHandlers;
@@ -25,8 +26,21 @@ namespace EventHighway.Portal.Web.Services.Views.EventHandlers
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<List<EventHandlerView>> RetrieveAllEventHandlersAsync(
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+        public async ValueTask<List<EventHandlerView>> RetrieveAllEventHandlersAsync(
+            CancellationToken cancellationToken = default)
+        {
+            IEnumerable<EventHandlerV2> eventHandlerV2s =
+                await this.eventHighwayBroker.RetrieveAllEventHandlerV2sAsync(
+                    cancellationToken);
+
+            return eventHandlerV2s.Select(AsView).ToList();
+        }
+
+        private static EventHandlerView AsView(EventHandlerV2 eventHandlerV2) =>
+            new EventHandlerView
+            {
+                Id = eventHandlerV2.Id,
+                Name = eventHandlerV2.Name
+            };
     }
 }
