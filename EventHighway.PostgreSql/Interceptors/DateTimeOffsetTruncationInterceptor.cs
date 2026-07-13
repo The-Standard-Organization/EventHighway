11657f94c339e99a969fb33276c230f00c4965e5
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-07-13 17:16:32 +0100

    Revert "CODERUB: Added saveChanges interceptor"
    This reverts commit 8e18a996534ed2a8e2b1fe6b2ed28d1c713efcdc.

--- EventHighway.PostgreSql/Interceptors/DateTimeOffsetTruncationInterceptor.cs
+++ EventHighway.PostgreSql/Interceptors/DateTimeOffsetTruncationInterceptor.cs
@@ -11,26 +11,13 @@ namespace EventHighway.PostgreSql.Interceptors
 {
     internal sealed class DateTimeOffsetTruncationInterceptor : SaveChangesInterceptor
     {
-        public override InterceptionResult<int> SavingChanges(
-            DbContextEventData eventData,
-            InterceptionResult<int> result)
-        {
-            if (eventData.Context is not null)
-                TruncateDateTimeOffsets(eventData.Context);
-
-            return base.SavingChanges(eventData, result);
-        }
-
         public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
             DbContextEventData eventData,
             InterceptionResult<int> result,
             CancellationToken cancellationToken = default)
         {
-            cancellationToken.ThrowIfCancellationRequested();
+            TruncateDateTimeOffsets(eventData.Context);
 
-            if (eventData.Context is not null) 
-                TruncateDateTimeOffsets(eventData.Context);
-            
             return base.SavingChangesAsync(eventData, result, cancellationToken);
         }
 
