8b7661c6d3338a0e18c59acff7c1ebde2d4ca409
Christo du Toit <christo@dutoit.co.uk>
2026-07-17 16:42:28 +0100

    ShouldThrowValidationExceptionOnRetrieveByQueryIfQueryIsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/V2/EventV2CoordinationServiceTests.Validations.RetrieveByQuery.cs
@@ -0,0 +1,69 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Coordinations.Events.V2;
+using EventHighway.Core.Models.Services.Coordinations.Events.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Coordinations.V2
+{
+    public partial class EventV2CoordinationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByQueryIfQueryIsNullAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventV2Query nullEventV2Query = null;
+
+            var nullEventV2QueryCoordinationException =
+                new NullEventV2QueryCoordinationException(
+                    message: "Event query is null.");
+
+            var expectedEventV2CoordinationValidationException =
+                new EventV2CoordinationValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: nullEventV2QueryCoordinationException);
+
+            // when
+            ValueTask<IQueryable<EventV2>> retrieveEventV2sByQueryTask =
+                this.eventV2CoordinationService.RetrieveEventV2sByQueryAsync(
+                    nullEventV2Query,
+                    randomCancellationToken);
+
+            EventV2CoordinationValidationException
+                actualEventV2CoordinationValidationException =
+                    await Assert.ThrowsAsync<EventV2CoordinationValidationException>(
+                        retrieveEventV2sByQueryTask.AsTask);
+
+            // then
+            actualEventV2CoordinationValidationException.Should()
+                .BeEquivalentTo(expectedEventV2CoordinationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2CoordinationValidationException))),
+                        Times.Once);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventFiringV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventParticipantV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
