6dd123cbd4f8183f2225914468a9b05db1628ee0
Christo du Toit <christo@dutoit.co.uk>
2026-07-18 05:05:25 +0100

    ShouldNotReprocessAlreadyAttemptedReplayedListenerEventV2sAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Logic.ProcessReplayed.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Logic.ProcessReplayed.cs
@@ -157,5 +157,66 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.ReplayingEvents.V2
             this.configurationBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldNotReprocessAlreadyAttemptedReplayedListenerEventV2sAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int randomTake = GetRandomNumber();
+            BatchConfiguration batchConfiguration = CreateBatchConfiguration(randomTake);
+
+            List<ListenerEventV2> persistentListenerEventV2Batch =
+                CreateRandomListenerEventV2s(randomTake);
+
+            this.configurationBrokerMock.Setup(broker =>
+                broker.GetBatchConfiguration())
+                    .Returns(batchConfiguration);
+
+            this.replayingListenerEventV2OrchestrationServiceMock
+                .SetupSequence(service =>
+                    service.RetrieveBatchOfReplayListenerEventV2sAsync(
+                        randomTake, randomCancellationToken))
+                .ReturnsAsync(persistentListenerEventV2Batch)
+                .ReturnsAsync(persistentListenerEventV2Batch)
+                .ReturnsAsync(new List<ListenerEventV2>());
+
+            foreach (ListenerEventV2 listenerEventV2 in persistentListenerEventV2Batch)
+            {
+                this.replayingListenerEventV2OrchestrationServiceMock.Setup(service =>
+                    service.ProcessReplayListenerEventV2Async(
+                        listenerEventV2, randomCancellationToken))
+                    .ReturnsAsync(listenerEventV2);
+            }
+
+            // when
+            await this.replayingEventV2CoordinationService
+                .ProcessReplayedListenerEventV2sAsync(randomCancellationToken);
+
+            // then
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetBatchConfiguration(), Times.Once);
+
+            this.replayingListenerEventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveBatchOfReplayListenerEventV2sAsync(
+                    randomTake, randomCancellationToken),
+                Times.Exactly(2));
+
+            foreach (ListenerEventV2 listenerEventV2 in persistentListenerEventV2Batch)
+            {
+                this.replayingListenerEventV2OrchestrationServiceMock.Verify(service =>
+                    service.ProcessReplayListenerEventV2Async(
+                        listenerEventV2, randomCancellationToken),
+                    Times.Once);
+            }
+
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.restoringEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.replayingListenerEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
