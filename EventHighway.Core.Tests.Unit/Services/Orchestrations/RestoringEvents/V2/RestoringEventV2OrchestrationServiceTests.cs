eac8f5e4750a32ae8bcd3238cf96c5847b766905
Christo du Toit <christo@dutoit.co.uk>
2026-07-18 05:10:06 +0100

    ShouldPreserveEventParticipantV2IdWhenRestoringEventV2Async -> PASS

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/RestoringEvents/V2/RestoringEventV2OrchestrationServiceTests.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/RestoringEvents/V2/RestoringEventV2OrchestrationServiceTests.cs
@@ -144,7 +144,8 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.RestoringEvents.V
                 UpdatedDate = eventArchiveV2.UpdatedDate,
                 ScheduledDate = null,
                 ContentHash = eventArchiveV2.ContentHash,
-                EventAddressV2Id = eventArchiveV2.EventAddressV2Id
+                EventAddressV2Id = eventArchiveV2.EventAddressV2Id,
+                EventParticipantV2Id = eventArchiveV2.EventParticipantV2Id
             };
 
         private ListenerEventV2 MapToListenerEventV2(ListenerEventArchiveV2 listenerEventArchiveV2) =>
