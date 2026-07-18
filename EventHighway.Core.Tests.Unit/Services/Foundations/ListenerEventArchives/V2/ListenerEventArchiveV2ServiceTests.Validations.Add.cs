ef22d97fc631a2dd2b210d1bd468432c1225b200
Christo du Toit <christo@dutoit.co.uk>
2026-07-18 03:20:53 +0100

    ShouldThrowValidationExceptionOnAddIfEventParticipantV2IdIsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEventArchives/V2/ListenerEventArchiveV2ServiceTests.Validations.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEventArchives/V2/ListenerEventArchiveV2ServiceTests.Validations.Add.cs
@@ -230,6 +230,72 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEventArchive
         }
 
         [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfEventParticipantV2IdIsInvalidAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+
+            ListenerEventArchiveV2 randomListenerEventArchiveV2 =
+                CreateRandomListenerEventArchiveV2(date: randomDateTimeOffset);
+
+            ListenerEventArchiveV2 invalidListenerEventArchiveV2 = randomListenerEventArchiveV2;
+            invalidListenerEventArchiveV2.EventParticipantV2Id = Guid.Empty;
+
+            var invalidListenerEventArchiveV2Exception =
+                new InvalidListenerEventArchiveV2Exception(
+                    message: "Listener event archive is invalid, fix the errors and try again.");
+
+            invalidListenerEventArchiveV2Exception.AddData(
+                key: nameof(ListenerEventArchiveV2.EventParticipantV2Id),
+                values: "Required");
+
+            var expectedListenerEventArchiveV2ValidationException =
+                new ListenerEventArchiveV2ValidationException(
+                    message: "Listener event archive validation error occurred, fix the errors and try again.",
+                    innerException: invalidListenerEventArchiveV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<ListenerEventArchiveV2> addListenerEventArchiveV2Task =
+                this.listenerEventArchiveV2Service.AddListenerEventArchiveV2Async(
+                    invalidListenerEventArchiveV2,
+                    randomCancellationToken);
+
+            ListenerEventArchiveV2ValidationException actualListenerEventArchiveV2ValidationException =
+                await Assert.ThrowsAsync<ListenerEventArchiveV2ValidationException>(
+                    addListenerEventArchiveV2Task.AsTask);
+
+            // then
+            actualListenerEventArchiveV2ValidationException.Should().BeEquivalentTo(
+                expectedListenerEventArchiveV2ValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventArchiveV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertListenerEventArchiveV2Async(
+                    It.IsAny<ListenerEventArchiveV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotSameAsUpdatedDateAndLogItAsync()
         {
             // given
