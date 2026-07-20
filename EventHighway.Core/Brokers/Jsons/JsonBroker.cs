1670b02ed361bc693450677e2b66d6cea552b3fa
Christo du Toit <christo@dutoit.co.uk>
2026-07-20 14:19:50 +0100

    ShouldGetNestedJsonPropertyValueByDotPath -> PASS

--- EventHighway.Core/Brokers/Jsons/JsonBroker.cs
+++ EventHighway.Core/Brokers/Jsons/JsonBroker.cs
@@ -20,16 +20,49 @@ namespace EventHighway.Core.Brokers.Jsons
         {
             using JsonDocument document = JsonDocument.Parse(json);
 
-            return document.RootElement.TryGetProperty(propertyName, out JsonElement element)
-                ? element.GetString()
+            return TryResolveProperty(document.RootElement, propertyName, out JsonElement element)
+                ? GetElementValue(element)
                 : null;
         }
 
         public bool CheckIfPropertyExist(string json, string propertyName)
         {
             using JsonDocument document = JsonDocument.Parse(json);
-            return document.RootElement.TryGetProperty(propertyName, out _);
+            return TryResolveProperty(document.RootElement, propertyName, out _);
         }
+
+        private static bool TryResolveProperty(
+            JsonElement rootElement,
+            string propertyName,
+            out JsonElement resolvedElement)
+        {
+            // An exact property name match takes precedence over dot-path traversal so
+            // properties whose names contain dots keep resolving as they always have.
+            if (rootElement.TryGetProperty(propertyName, out resolvedElement))
+                return true;
+
+            resolvedElement = rootElement;
+
+            foreach (string pathSegment in propertyName.Split('.'))
+            {
+                if (resolvedElement.ValueKind != JsonValueKind.Object
+                    || !resolvedElement.TryGetProperty(pathSegment, out resolvedElement))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static string GetElementValue(JsonElement element) =>
+            element.ValueKind switch
+            {
+                JsonValueKind.String => element.GetString(),
+                JsonValueKind.Null => null,
+                JsonValueKind.Undefined => null,
+                _ => element.GetRawText()
+            };
 
         public bool IsValidJson(string content)
         {
